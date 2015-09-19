9ede6a08df6366155a80025d5244318b985b8b78
rycwilson <rycwilson@gmail.com>
2015-09-18 21:57:25 -0700

    mvp-theme login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'site#index_mvp'
+  root 'site#index'
 
   devise_for :users
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
