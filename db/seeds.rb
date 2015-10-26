363b850b2114dada9727bdb48fdb9af8aa568f78
dlindblodev <dlindblodev@gmail.com>
2015-10-26 00:52:50 +0000

    added clicky tracking and hid signup links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Company.destroy_all
+Company.destroy_all
 Customer.destroy_all
 Story.destroy_all
 
