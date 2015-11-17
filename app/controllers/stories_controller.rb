0f820f2862039563dc704c98f422e4ac1c7fa7ae
Ryan Wilson <rycwilson@gmail.com>
2015-11-17 09:10:10 -0800

    tagging fixes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -29,7 +29,7 @@ class StoriesController < ApplicationController
     @company = Company.find params[:id]
     new_story = params[:story]
     # was a new customer entered? ...
-    new_customer = new_story[':customer'] if new_story[':customer'].to_i == 0
+    new_customer = new_story[:customer] if new_story[:customer].to_i == 0
     if new_customer
       customer = Customer.new name: new_customer, company_id: @company.id
       if customer.save
@@ -38,10 +38,10 @@ class StoriesController < ApplicationController
         puts 'problem creating Customer'
       end
     else  # existing customer
-      success = Success.new customer_id: new_story[':customer']
+      success = Success.new customer_id: new_story[:customer]
     end
     if success.save
-      story = Story.new title: new_story[':title'], success_id: success.id
+      story = Story.new title: new_story[:title], success_id: success.id
       if story.save
         assign_tags story, new_story
         redirect_to edit_story_path story
@@ -87,8 +87,8 @@ class StoriesController < ApplicationController
 
   def assign_tags story, new_story
 
-    if new_story[':industry_tags']
-      new_story[':industry_tags'].each do |selection|
+    if new_story[:industry_tags]
+      new_story[:industry_tags].each do |selection|
         if selection.to_i == 0   # if it's a generic tag
           # create a new company industry category based on the generic tag
           story.success.industry_categories << IndustryCategory.create(
@@ -99,14 +99,14 @@ class StoriesController < ApplicationController
       end
     end
 
-    if new_story[':product_cat_tags']
-      new_story[':product_cat_tags'].each do |selection|
+    if new_story[:product_cat_tags]
+      new_story[:product_cat_tags].each do |selection|
         story.success.product_categories << ProductCategory.find(selection)
       end
     end
 
-    if new_story[':product_tags']
-      new_story[':product_tags'].each do |selection|
+    if new_story[:product_tags]
+      new_story[:product_tags].each do |selection|
         story.success.products << Product.find(selection)
       end
     end
@@ -118,40 +118,47 @@ class StoriesController < ApplicationController
     old_product_cat_tags = story.success.product_categories
     old_product_tags = story.success.products
     # add new industry tags ...
-    new_tags[:industry_tags].each do |industry_id|
-      unless old_industry_tags.any? { |industry| industry.id == industry_id.to_i }
-        story.success.industry_categories << IndustryCategory.find(industry_id.to_i)
+    unless new_tags[:industry_tags].nil?
+      new_tags[:industry_tags].each do |industry_id|
+        unless old_industry_tags.any? { |industry| industry.id == industry_id.to_i }
+          story.success.industry_categories << IndustryCategory.find(industry_id.to_i)
+        end
       end
     end
     # remove deleted industry tags ...
     old_industry_tags.each do |industry|
-      unless new_tags[:industry_tags].include? industry.id.to_s
+      unless (new_tags[:industry_tags]) && (new_tags[:industry_tags].include? industry.id.to_s)
         IndustriesSuccess.where("success_id = ? AND industry_category_id = ?",
                                   story.success.id, industry.id)[0].destroy
       end
     end
     # add new product category tags ...
-    new_tags[:product_cat_tags].each do |product_cat_id|
-      unless old_product_cat_tags.any? { |product_cat| product_cat.id == product_cat_id.to_i }
-        story.success.product_categories << ProductCategory.find(product_cat_id.to_i)
+    unless new_tags[:product_cat_tags].nil?
+      new_tags[:product_cat_tags].each do |product_cat_id|
+        unless old_product_cat_tags.any? { |product_cat| product_cat.id == product_cat_id.to_i }
+          story.success.product_categories << ProductCategory.find(product_cat_id.to_i)
+        end
       end
     end
     # remove deleted product category tags ...
     old_product_cat_tags.each do |product_cat|
-      unless new_tags[:product_cat_tags].include? product_cat.id.to_s
+      unless (new_tags[:product_cat_tags]) &&
+          (new_tags[:product_cat_tags].include? product_cat.id.to_s)
         ProductCatsSuccess.where("success_id = ? AND product_category_id = ?",
                                   story.success.id, product_cat.id)[0].destroy
       end
     end
     # add new product tags ...
-    new_tags[:product_tags].each do |product_id|
-      unless old_product_tags.any? { |product| product.id == product_id.to_i }
-        story.success.products << Product.find(product_id.to_i)
+    unless new_tags[:product_tags].nil?
+      new_tags[:product_tags].each do |product_id|
+        unless old_product_tags.any? { |product| product.id == product_id.to_i }
+          story.success.products << Product.find(product_id.to_i)
+        end
       end
     end
     # remove deleted product tags ...
     old_product_tags.each do |product|
-      unless new_tags[:product_tags].include? product.id.to_s
+      unless (new_tags[:product_tags]) && (new_tags[:product_tags].include? product.id.to_s)
         ProductsSuccess.where("success_id = ? AND product_id = ?",
                                   story.success.id, product.id)[0].destroy
       end
