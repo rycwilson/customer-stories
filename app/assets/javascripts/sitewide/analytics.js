9b8264a468bb51c958dee859d12408b0f5703226
dlindblodev <dlindblodev@gmail.com>
2015-11-18 13:25:30 +0000

    updated promote and measure pages

--- app/assets/javascripts/sitewide/analytics.js
+++ app/assets/javascripts/sitewide/analytics.js
@@ -1,4 +1,4 @@
-app/assets/javascripts/analytics.js
+app/assets/javascripts/sitewide/analytics.js
 
 // http://stackoverflow.com/questions/18632644/google-analytics-with-rails-4
 // https://clicky.com/help/apps-plugins#rails4turbo
