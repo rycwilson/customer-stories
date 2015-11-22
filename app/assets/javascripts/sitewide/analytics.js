1970a429b9f3dcbb33d491dbae98de4027c70c82
dlindblodev <dlindblodev@gmail.com>
2015-11-22 05:18:20 +0000

    removed getclicky analytics js

--- app/assets/javascripts/sitewide/analytics.js
+++ app/assets/javascripts/sitewide/analytics.js
@@ -1,12 +1,7 @@
-app/assets/javascripts/sitewide/analytics.js
 
 // http://stackoverflow.com/questions/18632644/google-analytics-with-rails-4
 // https://clicky.com/help/apps-plugins#rails4turbo
 
 // Javascript
-$(document).on 'page:change', ->
-  if window.clicky?
-    clicky.log( document.location.pathname + document.location.search, document.title, 'pageview' );
 
-    
 
