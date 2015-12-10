3fc3b145ee9e6685cab3e231f94bbad579afade9
Ryan Wilson <rycwilson@gmail.com>
2015-12-10 09:38:53 -0800

    heroku/sendgrid experiment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ApplicationMailer
       default from: 'noreply@customerstories.net'
- 
+
   def cron_email()
- 
+
     mail(to: '***REMOVED***', subject: 'testing cron emailer')
   end
-    
+
+  def contribution_invite email
+
+    mail(to: email, subject: 'Tell us about your success')
+
+  end
+
 end
