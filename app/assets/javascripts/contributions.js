1b4a7a164f96da417b176dd3a233f900d0d97086
Ryan Wilson <rycwilson@gmail.com>
2016-02-19 21:29:53 -0800

    complete responsiveness for contribution submission page

--- app/assets/javascripts/contributions.js
+++ app/assets/javascripts/contributions.js
@@ -9,26 +9,26 @@
 */
 (function($, viewport){
 
-    $(function () {
+    // $(function () {
 
-      var $submit = $('#contribution-submission-form').find("input[type='submit']");
+    //   var $submit = $('#contribution-submission-form').find("input[type='submit']");
 
-      if( viewport.is('xs') ) {
-        if (!$submit.hasClass('btn-block')) {
-          $submit.addClass('btn-block');
-        }
-      }
+    //   if( viewport.is('xs') ) {
+    //     if (!$submit.hasClass('btn-block')) {
+    //       $submit.addClass('btn-block');
+    //     }
+    //   }
 
-      // Execute code each time window size changes
-      $(window).resize(
-        viewport.changed(function () {
-          if( viewport.is('xs') ) {
-            if (!$submit.hasClass('btn-block')) {
-              $submit.addClass('btn-block');
-            }
-          }
-        })
-      );
-    });
+    //   // Execute code each time window size changes
+    //   $(window).resize(
+    //     viewport.changed(function () {
+    //       if( viewport.is('xs') ) {
+    //         if (!$submit.hasClass('btn-block')) {
+    //           $submit.addClass('btn-block');
+    //         }
+    //       }
+    //     })
+    //   );
+    // });
 
 })(jQuery, ResponsiveBootstrapToolkit);
