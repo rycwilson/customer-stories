16cfaa5907c20ed0bf2325c6cf6d4698cba5677c
Ryan Wilson <rycwilson@gmail.com>
2016-03-03 14:39:59 -0800

    submission page styling changes

--- app/assets/javascripts/contributions.js
+++ app/assets/javascripts/contributions.js
@@ -1,31 +1,26 @@
 //= require responsive-toolkit/dist/bootstrap-toolkit
 
 /*
-  function monitors viewport size change and triggers callbacks
-  at pre-defined bootstrap breakpoints
-
   ref: http://stackoverflow.com/questions/18575582
        https://github.com/maciej-gurban/responsive-bootstrap-toolkit
 */
 
-
 $(function () {
 
-  $('#pointer-toggle').on('click', function () {
+  $('.accordian-toggle').on('click', function () {
     var $link = $(this).find('a');
-    if ($link.text() == 'show pointers')
+    if ($link.text() == 'helpful pointers')
       $link.text('hide pointers');
     else
-      $link.text('show pointers');
+      $link.text('helpful pointers');
   });
 
   // this will only run on document load; no dynamic checking
   (function($, viewport){
-
     if( viewport.is('xs') ) {
       $('.container').css('padding', '0');
+      $('.container > .col-xs-12').css('padding', '30px');
     }
-
   })(jQuery, ResponsiveBootstrapToolkit);
 
 });
