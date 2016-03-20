f72c71b9025e9db2122a371129c042a8e8e36506
Ryan Wilson <rycwilson@gmail.com>
2016-03-19 17:33:02 -0700

    modifies contribution template

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -10,9 +10,13 @@ module StoriesHelper
   end
 
   # extra parameter is necessary because user not connected to customer
-  def research_user_query_string user, customer
+  def research_user_query_string user, customer=nil
     user = User.find_by(id: user.id)
-    user.first_name + "+" + user.last_name + "+" + customer.name
+    if customer.present?
+      user.first_name + "+" + user.last_name + "+" + customer.name
+    else
+      user.first_name + "+" + user.last_name
+    end
   end
 
 end
