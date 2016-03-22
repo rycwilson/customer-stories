753c4b971aa19e24722b3d343b85188d640f9cd6
Ryan Wilson <rycwilson@gmail.com>
2016-03-22 12:57:15 -0700

    reverts some changes from last commit

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,7 +1,7 @@
 class Subdomain
 
   def self.matches? request
-    request.subdomain.present? && (request.subdomain !=~ /(www|csp-staging|floating-spire-2927)/) && Company.any? { |c| c.subdomain == request.subdomain }
+    request.subdomain.present? && (request.subdomain != 'www') && Company.any? { |c| c.subdomain == request.subdomain }
   end
 
 end
