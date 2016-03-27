6c8104cf8151dab77ac8406b4fe2555c464a2b25
dlindblodev <dlindblodev@gmail.com>
2016-03-26 17:15:59 -0700

    Update email_templates.rb
    Updated mail templates.  All templates are now populated.

--- db/seeds/email_templates.rb
+++ db/seeds/email_templates.rb
@@ -1,139 +1,146 @@
 
 module EmailTemplatesSeed
 
-  REQUEST_SUBJECT = "Participate in a [customer_name]/[company_name] success story"
-
+  REQUEST_SUBJECT = "[customer_name] and [company_name] success story"
   CUSTOMER_REQUEST_BODY = "<p>[contributor_first_name],</p>
-
 <p>[referral_intro]</p>
-
-<p>I’m reaching out to request a favor. I’m planning to write a success story about your project utilizing [product_name] from [company_name] and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
-
-<p>By participating, you can also use the curated success story as proof of your great work. It’s a great way to build your professional brand and showcase impact you’ve delivered. It will also be used as a reference to help others learn about solving similar needs.</p>
-
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about your project utilizing Cisco Systems and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>This story will also benefit you as it’s a success showcasing your accomplishments. It’s a great way to build your professional brand. You will also help peers make informed decisions when trying to solve similar needs.</p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
-
 <p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your colleagues.</p>
-
 <p>Thank you,</p>
-
-<img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
-
+<img id='curator-img' src='[curator_img_url]' style='width:80px' alt='curator-img'> <br>
 [curator_full_name] <br>
 [curator_title] <br>
 [company_name] <br>
 [curator_email] | [curator_phone] <br>
-
-<p>Note: I will be sure to get approval from [customer_name] before publishing.</p>
-
 <p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
-
 <p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
-  CUSTOMER_REMIND1_SUBJECT = "Reminder: participate in a [customer_name]/[company_name] success story"
-
+  CUSTOMER_REMIND1_SUBJECT = "Reminder: [customer_name] and [company_name] success story"
   CUSTOMER_REMIND1_BODY = "<p>[contributor_first_name],</p>
+<p>[referral_intro]</p>
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about your project utilizing Cisco Systems and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>This story will also benefit you as it’s a success showcasing your accomplishments. It’s a great way to build your professional brand. You will also help peers make informed decisions when trying to solve similar needs.</p>
+<p> <a href='[contribution_url]'>Provide your input here</a> </p>
+<p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your colleagues.</p>
+<p>Thank you,</p>
+<img id='curator-img' src='[curator_img_url]' style='width:80px' alt='curator-img'> <br>
+[curator_full_name] <br>
+[curator_title] <br>
+[company_name] <br>
+[curator_email] | [curator_phone] <br>
+<p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
-<p>This is your first reminder to make a contribution.</p>
-
-<p> <a href='[contribution_url]'>Contribute</a> </p>
-<p> <a href='[feedback_url]'>Feedback</a> </p>
-<p> <a href='[opt_out_url]'>Opt-out</a> </p>"
-
-  CUSTOMER_REMIND2_SUBJECT = "Final reminder: participate in a [customer_name]/[company_name] success story"
-
+  CUSTOMER_REMIND2_SUBJECT = "Final reminder: [customer_name] and [company_name] success story"
   CUSTOMER_REMIND2_BODY = "<p>[contributor_first_name],</p>
-
-<p>This is your final reminder to make a contribution.</p>
-
-<p> <a href='[contribution_url]'>Contribute</a> </p>
-<p> <a href='[feedback_url]'>Feedback</a> </p>
-<p> <a href='[opt_out_url]'>Opt-out</a> </p>"
+<p>[referral_intro]</p>
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about your project utilizing Cisco Systems and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>This story will also benefit you as it’s a success showcasing your accomplishments. It’s a great way to build your professional brand. You will also help peers make informed decisions when trying to solve similar needs.</p>
+<p> <a href='[contribution_url]'>Provide your input here</a> </p>
+<p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your colleagues.</p>
+<p>Thank you,</p>
+<img id='curator-img' src='[curator_img_url]' style='width:80px' alt='curator-img'> <br>
+[curator_full_name] <br>
+[curator_title] <br>
+[company_name] <br>
+[curator_email] | [curator_phone] <br>
+<p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
   PARTNER_REQUEST_BODY = "<p>[contributor_first_name],</p>
-
 <p>[referral_intro]</p>
-
-<p>I’m reaching out to request a favor. I’m planning to write a success story about your company’s service success with [customer_name] utilizing [product_name] from [company_name]. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
-
-<p>By participating, you can use the curated success story as personal validation of how you contributed to [company_name]’s success utilizing [company_name].   It’s a great reference to use with other prospects, proving your professional credibility in achieving results for your customers.</p>
-
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about the project with [customer_name] and [company_name]. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>By participating, you can use the success story as your professional validation of your contribution to [customer_name]’s success utilizing [company_name].   It’s a great reference to use with prospects, proving your personal credibility in achieving results for them.</p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
-
 <p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your prospects and colleagues.</p>
-
 <p>Thank you,</p>
-
 <img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
-
 [curator_full_name] <br>
 [curator_title] <br>
 [company_name] <br>
 [curator_email] | [curator_phone] <br>
-
 <p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
-
 <p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
-  PARTNER_REMIND1_SUBJECT = "Reminder: participate in a [customer_name]/[company_name] success story"
+  PARTNER_REMIND1_SUBJECT = "Reminder: [customer_name] and [company_name] success story"
   PARTNER_REMIND1_BODY = "<p>[contributor_first_name],</p>
+<p>[referral_intro]</p>
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about the project with [customer_name] and [company_name]. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>By participating, you can use the success story as your professional validation of your contribution to [customer_name]’s success utilizing [company_name].   It’s a great reference to use with prospects, proving your personal credibility in achieving results for them.</p>
+<p> <a href='[contribution_url]'>Provide your input here</a> </p>
+<p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your prospects and colleagues.</p>
+<p>Thank you,</p>
+<img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
+[curator_full_name] <br>
+[curator_title] <br>
+[company_name] <br>
+[curator_email] | [curator_phone] <br>
+<p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
-<p>This is your first reminder to make a contribution.</p>
-
-<p> <a href='[contribution_url]'>Contribute</a> </p>
-<p> <a href='[feedback_url]'>Feedback</a> </p>
-<p> <a href='[opt_out_url]'>Opt-out</a> </p>"
-
-  PARTNER_REMIND2_SUBJECT = "Final reminder: participate in a [customer_name]/[company_name] success story"
+  PARTNER_REMIND2_SUBJECT = "Final reminder: [customer_name] and [company_name] success story"
   PARTNER_REMIND2_BODY = "<p>[contributor_first_name],</p>
-
-<p>This is your final reminder to make a contribution.</p>
-
-<p> <a href='[contribution_url]'>Contribute</a> </p>
-<p> <a href='[feedback_url]'>Feedback</a> </p>
-<p> <a href='[opt_out_url]'>Opt-out</a> </p>"
+<p>[referral_intro]</p>
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about the project with [customer_name] and [company_name]. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>By participating, you can use the success story as your professional validation of your contribution to [customer_name]’s success utilizing [company_name].   It’s a great reference to use with prospects, proving your personal credibility in achieving results for them.</p>
+<p> <a href='[contribution_url]'>Provide your input here</a> </p>
+<p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your prospects and colleagues.</p>
+<p>Thank you,</p>
+<img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
+[curator_full_name] <br>
+[curator_title] <br>
+[company_name] <br>
+[curator_email] | [curator_phone] <br>
+<p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
   SALES_REQUEST_BODY = "<p>[contributor_first_name],</p>
-
 <p>[referral_intro]</p>
-
-<p>I’m reaching out to request a favor. I’m planning to write a success story about [customer_name] utilizing [product_name] from [company_name] and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
-
-<p>By participating, you can use the curated success story as personal validation of your contribution to [customer_name]’s success utilizing [company_name]. It’s a great reference to use with other prospects, proving your professional credibility in achieving results for your customers.</p>
-
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about [customer_name] utilizing [company_name] and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>By participating, you can use the curated success story as your professional validation of your contribution to [customer_name]’s success utilizing [company_name]. It’s a great reference to use with other prospects, proving your personal credibility in achieving results for them.</p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
-
 <p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your prospects and colleagues.</p>
-
 <p>Thank you,</p>
-
 <img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
-
 [curator_full_name] <br>
 [curator_title] <br>
 [company_name] <br>
 [curator_email] | [curator_phone] <br>
-
 <p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
-
 <p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
-  SALES_REMIND1_SUBJECT = "Reminder: participate in a [customer_name]/[company_name] success story"
+  SALES_REMIND1_SUBJECT = "Reminder: [customer_name] and [company_name] success story"
   SALES_REMIND1_BODY = "<p>[contributor_first_name],</p>
+<p>[referral_intro]</p>
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about [customer_name] utilizing [company_name] and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>By participating, you can use the curated success story as your professional validation of your contribution to [customer_name]’s success utilizing [company_name]. It’s a great reference to use with other prospects, proving your personal credibility in achieving results for them.</p>
+<p> <a href='[contribution_url]'>Provide your input here</a> </p>
+<p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your prospects and colleagues.</p>
+<p>Thank you,</p>
+<img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
+[curator_full_name] <br>
+[curator_title] <br>
+[company_name] <br>
+[curator_email] | [curator_phone] <br>
+<p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
-<p>This is your first reminder to make a contribution.</p>
-
-<p> <a href='[contribution_url]'>Contribute</a> </p>
-<p> <a href='[feedback_url]'>Feedback</a> </p>
-<p> <a href='[opt_out_url]'>Opt-out</a> </p>"
-
-  SALES_REMIND2_SUBJECT = "Final reminder: participate in a [customer_name]/[company_name] success story"
+  SALES_REMIND2_SUBJECT = "Final reminder: [customer_name] and [company_name] success story"
   SALES_REMIND2_BODY = "<p>Dear [contributor_first_name],</p>
-
-<p>This is your final reminder to make a contribution.</p>
-
-<p> <a href='[contribution_url]'>Contribute</a> </p>
-<p> <a href='[feedback_url]'>Feedback</a> </p>
-<p> <a href='[opt_out_url]'>Opt-out</a> </p>"
+<p>[referral_intro]</p>
+<p>I’m reaching out to request a favor. I’m planning on writing a success story about [customer_name] utilizing [company_name] and would like your input. I will professionally write the story and need just three minutes of your time jotting down some notes.</p>
+<p>By participating, you can use the curated success story as your professional validation of your contribution to [customer_name]’s success utilizing [company_name]. It’s a great reference to use with other prospects, proving your personal credibility in achieving results for them.</p>
+<p> <a href='[contribution_url]'>Provide your input here</a> </p>
+<p>Please call or email if you have any questions. I’m confident you will be proud of the curated story and will want to share with your prospects and colleagues.</p>
+<p>Thank you,</p>
+<img id='curator-img' src='[curator_img_url]' style='width:100px' alt='curator-img'> <br>
+[curator_full_name] <br>
+[curator_title] <br>
+[company_name] <br>
+[curator_email] | [curator_phone] <br>
+<p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
 end
