0b643be36d547a5a36d65d340c5dc0a17293aed0
Ryan Wilson <rycwilson@gmail.com>
2016-03-31 23:37:00 -0700

    fixes latest migration

--- db/migrate/20160401053450_remove_devise_indices_from_users.rb
+++ db/migrate/20160401053450_remove_devise_indices_from_users.rb
@@ -1,9 +1,9 @@
 class RemoveDeviseIndicesFromUsers < ActiveRecord::Migration
 
   def change
-    remove_index :users, :reset_password_token
-    remove_index :users, :unlock_token
-    remove_index :admins, :reset_password_token
+    remove_index :users, column: :reset_password_token
+    remove_index :users, column: :unlock_token
+    remove_index :admins, column: :reset_password_token
   end
 
 end
