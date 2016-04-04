fc3fe553d92b65d86cac4c85da76117ba7efee47
Ryan Wilson <rycwilson@gmail.com>
2016-04-04 15:20:30 -0700

    modifies submission confirmation page and email templates

--- db/seeds/email_templates.rb
+++ db/seeds/email_templates.rb
@@ -6,7 +6,7 @@ module EmailTemplatesSeed
   CUSTOMER_BODY = "<p>[contributor_first_name],</p>
 <p>[contributor_first_name],</p>
 <p>[referral_intro]</p>
-<p>We're writing a success story about your project using [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
+<p>We're writing a success story about your project using [company_name] and would appreciate three minutes of your time to jot down your input.</p>
 <p>The benefit to you is this story will showcase your accomplishments. </p>
 <p>It's a great reference to build your professional brand, and we'll make it easy for you to post it to your LinkedIn profile. You will also help peers make informed decisions when trying to solve similar needs. </p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
@@ -18,12 +18,12 @@ module EmailTemplatesSeed
 [company_name] <br>
 [curator_email] | [curator_phone] <br>
 <p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
-<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
   PARTNER_BODY = "<p>[contributor_first_name],</p>
 <p>[contributor_first_name],</p>
 <p>[referral_intro]</p>
-<p>We're writing a success story about [customer_name]'s use of [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
+<p>We're writing a success story about [customer_name]'s use of [company_name] and would appreciate three minutes of your time to jot down your input.</p>
 <p>The benefit to you is this story will highlight your contribution to [customer_name]’s success, and will give you a powerful reference story to help you build trust with prospects.</p>
 <p>It’s also a great way to build your professional brand, and we'll make it easy for you to post it to your LinkedIn profile. </p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
@@ -35,12 +35,12 @@ module EmailTemplatesSeed
 [company_name] <br>
 [curator_email] | [curator_phone] <br>
 <p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
-<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
   SALES_BODY = "<p>[contributor_first_name],</p>
 <p>[contributor_first_name],</p>
 <p>[referral_intro]</p>
-<p>We're writing a success story about [customer_name]'s use of [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
+<p>We're writing a success story about [customer_name]'s use of [company_name] and would appreciate three minutes of your time to jot down your input.</p>
 <p>The benefit to you is this story will highlight your contribution to [customer_name]’s success, and will give you a powerful reference story to help you build trust with prospects.</p>
 <p>It’s also a great way to build your professional brand, and we'll make it easy for you to post it to your LinkedIn profile. </p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
@@ -52,6 +52,6 @@ module EmailTemplatesSeed
 [company_name] <br>
 [curator_email] | [curator_phone] <br>
 <p>If you feel you can't contribute to a success story, please give us <a href='[feedback_url]'>feedback</a> why.</p>
-<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>
+<p>You can <a href='[unsubscribe_url]'>unsubscribe</a> from this success story or <a href='[opt_out_url]'>opt out</a> of all future requests.</p>"
 
 end
