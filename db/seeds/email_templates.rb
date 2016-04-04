2fd9cf560e706ee57e721d06ed81c95c4da4c1e7
dlindblodev <dlindblodev@gmail.com>
2016-04-03 17:08:16 -0700

    Update email_templates.rb
    Yet another slight update to the template.

--- db/seeds/email_templates.rb
+++ db/seeds/email_templates.rb
@@ -8,9 +8,9 @@ module EmailTemplatesSeed
 <p>[referral_intro]</p>
 <p>We're writing a success story about your project using [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
 <p>The benefit to you is this story will showcase your accomplishments. </p>
-<p>The success story will be a great reference to build your professional brand, and we will make it easy for you to post to your LinkedIn profile. You will also help peers make informed decisions when trying to solve similar needs. </p>
+<p>It's a great reference to build your professional brand, and we'll make it easy for you to post it to your LinkedIn profile. You will also help peers make informed decisions when trying to solve similar needs. </p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
-<p>Please call or email if you have any questions. I’m confident you will be proud of the finished story and will want to share with your colleagues.</p>
+<p>Please call or email with any questions. I’m confident you will be proud of the finished story and will want to share it with your colleagues.</p>
 <p>Thank you,</p>
 <img id='curator-img' src='[curator_img_url]' style='width:80px' alt='curator-img'> <br>
 [curator_full_name] <br>
@@ -23,11 +23,11 @@ module EmailTemplatesSeed
   PARTNER_BODY = "<p>[contributor_first_name],</p>
 <p>[contributor_first_name],</p>
 <p>[referral_intro]</p>
-<p>We're writing a success story about [customer_name] using [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
-<p>The benefit to you is this story will include your contribution to [customer_name]’s success.</p>
-<p>It’s a great way to build your professional brand, and we will make it easy for you to post to your LinkedIn profile. When working with new prospects it will be a powerful reference to help you build trust. </p>
+<p>We're writing a success story about [customer_name]'s use of [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
+<p>The benefit to you is this story will highlight your contribution to [customer_name]’s success, and will give you a powerful reference story to help you build trust with prospects.</p>
+<p>It’s also a great way to build your professional brand, and we'll make it easy for you to post it to your LinkedIn profile. </p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
-<p>Please call or email if you have any questions. I’m confident you will be proud of the finished story and will want to share with your prospects and colleagues.</p>
+<p>Please call or email me with any questions. I’m confident you will be proud of the finished story and will want to share with your prospects and colleagues.</p>
 <p>Thank you,</p>
 <img id='curator-img' src='[curator_img_url]' style='width:80px' alt='curator-img'> <br>
 [curator_full_name] <br>
@@ -40,11 +40,11 @@ module EmailTemplatesSeed
   SALES_BODY = "<p>[contributor_first_name],</p>
 <p>[contributor_first_name],</p>
 <p>[referral_intro]</p>
-<p>We're writing a success story about [customer_name] using [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
-<p>The benefit to you is this story will include your contribution to [customer_name]’s success.</p>
-<p>It’s a great way to build your professional brand, and we will make it easy for you to post to your LinkedIn profile. When working with new prospects it will be a powerful reference to help you build trust. </p>
+<p>We're writing a success story about [customer_name]'s use of [company_name] and would appreciate 3 minutes of your time to jot down your input.</p>
+<p>The benefit to you is this story will highlight your contribution to [customer_name]’s success, and will give you a powerful reference story to help you build trust with prospects.</p>
+<p>It’s also a great way to build your professional brand, and we'll make it easy for you to post it to your LinkedIn profile. </p>
 <p> <a href='[contribution_url]'>Provide your input here</a> </p>
-<p>Please call or email if you have any questions. I’m confident you will be proud of the finished story and will want to share with your prospects and colleagues.</p>
+<p>Please call or email me with any questions. I’m confident you will be proud of the finished story and will want to share with your prospects and colleagues.</p>
 <p>Thank you,</p>
 <img id='curator-img' src='[curator_img_url]' style='width:80px' alt='curator-img'> <br>
 [curator_full_name] <br>
