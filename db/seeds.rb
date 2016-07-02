5e2a90b11be953785406bed8161c677921b803ac
Ryan Wilson <rycwilson@gmail.com>
2016-07-02 11:18:06 -0700

    modifies seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,22 +20,22 @@ STATUS_OPTIONS = ['pre_request', 'request', 'remind1', 'remind2', 'feedback', 'c
 # Company.where.not("name IN ('CSP', 'Cisco Systems', 'Acme')").destroy_all
 
 # destroy CSP / Cisco / Acme data ...
-StoryCategory.destroy_all
-Product.destroy_all
-EmailTemplate.destroy_all
-Customer.destroy_all  # destroys successes
-                      # -> destroys stories, prompts, results, visitors,
-                      #    successes* join tables, contributions, email_contribution_requests
-User.destroy_all
-Company.destroy_all
+# StoryCategory.destroy_all
+# Product.destroy_all
+# EmailTemplate.destroy_all
+# Customer.destroy_all  # destroys successes
+#                       # -> destroys stories, prompts, results, visitors,
+#                       #    successes* join tables, contributions, email_contribution_requests
+# User.destroy_all
+# Company.destroy_all
 # User.where.not("email IN ('***REMOVED***', '***REMOVED***', '***REMOVED***', '***REMOVED***')").destroy_all
 
-# dan = User.find_by(email:'***REMOVED***')
-dan = User.create(first_name:'Dan', last_name:'Lindblom', email:'***REMOVED***', linkedin_url:'https://www.linkedin.com/in/danlindblom', sign_up_code:'csp_beta', password:'password', photo_url: 'https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/0e2caaaf-d808-4279-b7ca-9929cfc6400c/dan.png')
-# ryan = User.find_by(email:'***REMOVED***')
-# joe = User.find_by(email:'***REMOVED***')
+dan = User.find_by(email:'***REMOVED***')
+# dan = User.create(first_name:'Dan', last_name:'Lindblom', email:'***REMOVED***', linkedin_url:'https://www.linkedin.com/in/danlindblom', sign_up_code:'csp_beta', password:'password', photo_url: 'https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/0e2caaaf-d808-4279-b7ca-9929cfc6400c/dan.png')
+ryan = User.find_by(email:'***REMOVED***')
+# joe = User.find_by(email:'joe@mail.com')
 # frank = User.find_by(email:'***REMOVED***')
-ryan = User.create(first_name:'Ryan', last_name:'Wilson', email:'***REMOVED***', linkedin_url:'https://www.linkedin.com/in/wilsonryanc', sign_up_code:'csp_beta', password:'password', photo_url: 'https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/099b59d3-1f35-4d8b-9183-a162a80bfbac/ryan.png')
+# ryan = User.create(first_name:'Ryan', last_name:'Wilson', email:'***REMOVED***', linkedin_url:'https://www.linkedin.com/in/wilsonryanc', sign_up_code:'csp_beta', password:'password', photo_url: 'https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/099b59d3-1f35-4d8b-9183-a162a80bfbac/ryan.png')
 
 # acme = Company.find_by(name:'Acme')
 # acme.users.push(joe, frank)
@@ -47,11 +47,11 @@ ryan = User.create(first_name:'Ryan', last_name:'Wilson', email:'***REMOVED***',
 #                       photo_url:"https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/1c396f91-6232-4bdb-916e-907fcf6f6905/dlindblo[2].jpg",
 #                       phone:"650-327-6040", title:"Sales, Marketing, Entrepreneur, Investor, Advisor")
 
-# cisco = Company.find_by(name:'Cisco Systems')
-cisco = Company.create(name:'Cisco Systems', subdomain:'cisco', feature_flag:'demo', website: 'https://cisco.com',
-                   logo_url:'https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/6326ee57-e0e0-4a0b-aacb-9b59849f2c40/cisco-grey@2x.png',
-                   nav_color_1:'#007fc5', nav_color_2:'#2B5693' , nav_text_color:'#FCFCFD')
-cisco.users << dan << ryan
+cisco = Company.find_by(name:'Cisco Systems')
+# cisco = Company.create(name:'Cisco Systems', subdomain:'cisco', feature_flag:'demo', website: 'https://cisco.com',
+#                    logo_url:'https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/6326ee57-e0e0-4a0b-aacb-9b59849f2c40/cisco-grey@2x.png',
+#                    nav_color_1:'#007fc5', nav_color_2:'#2B5693' , nav_text_color:'#FCFCFD')
+cisco.users << ryan << dan
 
 # trunity = Company.create(name:'Trunity', subdomain:'trunity',
 #                     logo_url:"https://csp-production-assets.s3-us-west-1.amazonaws.com/uploads/7272f9a8-5a97-460f-b6c6-5b176e8880d3/trunity_logo.png",
@@ -68,9 +68,9 @@ cisco.users << dan << ryan
 # cg.users << User.create(email:'compas@customerstories.net', first_name:'Dan', last_name:'Lindblom', sign_up_code:'csp_beta', password:'password')
 
 # csp = Company.find_by(name:'CSP')
-csp = Company.create(name:'CSP', subdomain:'csp', feature_flag:'beta', website:'http://www.fark.com',
-                logo_url:"https://s3-us-west-1.amazonaws.com/csp-production-assets/cs_logo.png",
-                nav_color_1:"#FBFBFB", nav_color_2:"#85CEE6", nav_text_color:"#333333")
+# csp = Company.create(name:'CSP', subdomain:'csp', feature_flag:'beta', website:'http://www.fark.com',
+#                 logo_url:"https://s3-us-west-1.amazonaws.com/csp-production-assets/cs_logo.png",
+#                 nav_color_1:"#FBFBFB", nav_color_2:"#85CEE6", nav_text_color:"#333333")
 # csp.users << dan << ryan
 
 
@@ -96,15 +96,16 @@ end
 # end
 
 # Default email templates
-csp.email_templates << EmailTemplate.create(name: "Customer", subject: EmailTemplatesSeed::REQUEST_SUBJECT, body: EmailTemplatesSeed::CUSTOMER_BODY)
-csp.email_templates << EmailTemplate.create(name: "Partner", subject: EmailTemplatesSeed::REQUEST_SUBJECT, body: EmailTemplatesSeed::PARTNER_BODY)
-csp.email_templates << EmailTemplate.create(name: "Sales", subject: EmailTemplatesSeed::REQUEST_SUBJECT, body: EmailTemplatesSeed::SALES_BODY)
+# csp.email_templates << EmailTemplate.create(name: "Customer", subject: EmailTemplatesSeed::REQUEST_SUBJECT, body: EmailTemplatesSeed::CUSTOMER_BODY)
+# csp.email_templates << EmailTemplate.create(name: "Partner", subject: EmailTemplatesSeed::REQUEST_SUBJECT, body: EmailTemplatesSeed::PARTNER_BODY)
+# csp.email_templates << EmailTemplate.create(name: "Sales", subject: EmailTemplatesSeed::REQUEST_SUBJECT, body: EmailTemplatesSeed::SALES_BODY)
 
-Company.where.not(name:'CSP').each { |c| c.create_email_templates }
+# Company.where.not(name:'CSP').each { |c| c.create_email_templates }
 
 # Customers and Stories...
 
 def seed_company company, *users
+  company.customers.destroy_all
   DemoCustomersSeed::DEMO_CUSTOMERS.each do |customer_info|
     customer = Customer.create(name: customer_info[:name],
                            logo_url: customer_info[:logo],
