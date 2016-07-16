4d03ad3a6aa8cfbbd0dd00b7135e0c66664f3c13
Ryan Wilson <rycwilson@gmail.com>
2016-07-16 09:47:44 -0700

    adds thumb/inline video for mobile

--- app/assets/javascripts/stories/video.js
+++ app/assets/javascripts/stories/video.js
@@ -3,76 +3,77 @@ var VIDEO_LIB = {
   loadThumbnail: function () {
 
     var $thumbContainer = $(".video-thumb-container"),
+        $modal = $('#story-video-modal'),
         provider = $thumbContainer.data('provider'),
         videoId = $thumbContainer.data('video-id'),
         videoUrl = $thumbContainer.data("video-url"),
-        src = null;
+        videoQuery = '',
+        thumbSrc = '',
+        $player = null;
 
     if (provider === 'youtube') {
+      videoQuery = "?autoplay=1&autohide=2&&enablejsapi=1&controls=0&showinfo=0&rel=0";
       // 1, 2, 3, hqdefault, mqdefault, default
       // ref: http://stackoverflow.com/questions/2068344
-      src = '//img.youtube.com/vi/' + videoId + '/hqdefault.jpg';
+      thumbSrc = '//img.youtube.com/vi/' + videoId + '/hqdefault.jpg';
       $thumbContainer.append(
           "<div>" +
-            "<img class='video-thumb' src='" + src + "'>" +
+            "<img class='video-thumb' src='" + thumbSrc + "'>" +
             "<i class='fa fa-5x fa-inverse fa-play-circle-o'></i>" +
           "</div>");
+
     } else if (provider === 'vimeo' ) {
+      videoQuery = "?autoplay=1";
       // 640 (large), 200x150 (medium), or 100x75 (small)
       // ref: http://stackoverflow.com/questions/1361149/
       // (can't use a direct url because there's a different id to get to the thumbnails)
       $.getJSON('//vimeo.com/api/v2/video/' + videoId + '.json', function (data, status) {
-        src = data[0].thumbnail_medium;
+        thumbSrc = data[0].thumbnail_medium;
         $thumbContainer.append(
           "<div>" +
-            "<img class='video-thumb' src='" + src + "'>" +
-            "<div class='fa fa-5x fa-inverse fa-play-circle-o'></div>" +
+            "<img class='video-thumb' src='" + thumbSrc + "'>" +
+            "<i class='fa fa-5x fa-inverse fa-play-circle-o'></i>" +
           "</div>");
       });
     }
+    $thumbContainer.on('click', 'img, i', function (e) {
 
-    $thumbContainer.on('click', function () {
+      if ($(document).width() < 600) {
 
-      // if ($(document).width < 600) {
+        e.stopPropagation();  // stop modal from being triggered
 
-      //   var $iframe = $('iframe'),
-      //       query = "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0";
+        var $iframe = $('<iframe></iframe>');
 
-      //   if (provider === 'youtube') {
-      //     $iframe.attr('src', "//www.youtube.com/embed/" + videoId + query);
-      //   } else if (provider === 'vimeo') {
-      //     $iframe.attr('src', "//player.vimeo.com/video/" + videoId + query);
-      //   }
-      //   $iframe.attr('src', src)
-      //          .attr('frameborder', '0')
+        if (provider === 'youtube') {
+          $iframe.attr('src', "//www.youtube.com/embed/" + videoId + videoQuery);
+        } else if (provider === 'vimeo') {
+          $iframe.attr('src', "//player.vimeo.com/video/" + videoId + videoQuery);
+        }
+        $iframe.attr('frameborder', '0');
+        $(this).parent().replaceWith($iframe);
 
-      //   iframe.setAttribute("src", "//www.youtube.com/embed/" + this.parentNode.dataset.id + "?autoplay=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&controls=0&showinfo=0");
-      //   iframe.setAttribute("frameborder", "0");
-      //   iframe.setAttribute("id", "youtube-iframe");
-      //   this.parentNode.replaceChild(iframe, this);
-
-      // } else {
-
-        var $modal = $('#story-video-modal'),
-            $player = $modal.find('iframe'),
-            videoUrl = $(this).data("video-url"),
-            videoUrlAuto = videoUrl + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+      } else { // play video in modal
+        var $player = $modal.find('iframe'),
+            videoUrlAuto = videoUrl + videoQuery;
 
         $player.attr('src', videoUrlAuto);
 
         $modal.find('button.close').on('click', function () {
-          $player.attr('src', videoUrl);
+          $player.attr('src', '');
         });
 
         $modal.on('hidden.bs.modal', function () {
           $player.attr('src', '');
         });
-
-      // }
-
+      }
 
     });
-
-  }
+  } // loadThumbnail
 
 };
+
+
+
+
+
+
