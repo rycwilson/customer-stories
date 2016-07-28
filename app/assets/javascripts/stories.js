b6728b17fdb0f51ac5c959dc266b9320e8058814
Ryan Wilson <rycwilson@gmail.com>
2016-07-28 16:52:04 -0700

    adds social share static links and popup window

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -49,7 +49,8 @@ $(window).on('load', function () {
 
 function ready () {
 
-  initPlugins() ;
+  initPlugins();
+  initSocialShare();
   configUnderscore();
 
   VIDEO_LIB.loadThumbnail();
@@ -133,7 +134,44 @@ function initPlugins () {
       ['help', ['help']]
     ],
   });
+}
 
+// ref: https://codepen.io/patrickkahl/pen/DxmfG
+// ref: http://stackoverflow.com/questions/4068373/center-a-popup-window-on-screen
+function initSocialShare() {
+
+  $('#social-buttons .linkedin').on('click', function (e) {
+    $(this).socialSharePopup(e, 550, 442);
+  });
+  $('#social-buttons .twitter').on('click', function (e) {
+    $(this).socialSharePopup(e, 550, 253);
+  });
+  $('#social-buttons .facebook').on('click', function (e) {
+    $(this).socialSharePopup(e, 560, 442);
+  });
+
+  $.fn.socialSharePopup = function (e, width, height) {
+    // Prevent default anchor event
+    e.preventDefault();
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+
+    var windowWidth = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var windowHeight = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    // Set values for window
+    width = width || '550';
+    height = height || '442';
+
+    var left = ((windowWidth / 2) - (width / 2)) + dualScreenLeft;
+    var top = ((windowHeight / 2) - (height / 2)) + dualScreenTop;
+
+    // Set title and open popup with focus on it
+    var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
+        strParam = 'width=' + width + ',height=' + height + ',top=' + top + ',left=' + left + ',resizable=no',
+        objWindow = window.open(this.attr('href'), 'shareWindow', strParam).focus();
+  };
 }
 
 function initLinkedIn () {
@@ -141,7 +179,7 @@ function initLinkedIn () {
   // linkedin widgets (load IN.js library conditionally)
   if (typeof(IN) !== "object") {
     console.log("loading in.js ...");
-    $.getScript('//platform.linkedin.com/in.js');
+    $.getScript('//platform.linkedin.com/in.js?async=true');
   } else {
     console.log("in.js already loaded");
   }
