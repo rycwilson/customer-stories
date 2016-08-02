c2135d781da11746b523a02e9854cffb0dbcadd6
Ryan Wilson <rycwilson@gmail.com>
2016-08-02 13:34:06 -0700

    modifies og tags

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,6 @@
 INDUSTRIES = ['Education', 'Government', 'Financial Services', 'Healthcare', 'Hospitality', 'Manufacturing', 'Media and Entertainment', 'Service Provider', 'Technology', 'IT', 'Telecommunications'];
 
+CS_LOGO_URL = "https://s3-us-west-1.amazonaws.com/csp-production-assets/cs_logo.png"
 
+CS_FULL_LOGO_URL = "https://s3-us-west-1.amazonaws.com/csp-production-assets/cs_logo_full_4.png"
 
