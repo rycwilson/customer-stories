92253979659689a800752907c52624f8545cdae2
Ryan Wilson <rycwilson@gmail.com>
2016-08-05 18:02:44 -0700

    fixes promote section styling adjustments

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -215,7 +215,6 @@ function configUnderscore() {
 }
 
 
-
 function editTagsListeners () {
   /*
     Remember the initial <option>s of the tag select inputs
@@ -252,19 +251,6 @@ function editTagsListeners () {
     // tagsFormDirty = false;
   });
 
-}
-
-/**
- * Get the value of a querystring
- * @param  {String} field The field to get the value of
- * @param  {String} url   The URL to get the value from (optional)
- * @return {String}       The field value
- */
-function getQueryString (field, url) {
-    var href = url ? url : window.location.href;
-    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
-    var string = reg.exec(href);
-    return string ? string[1] : null;
 }
 
 /**
@@ -807,6 +793,8 @@ function centerLogos () {
     }
   });
 }
+
+
 
 
 
