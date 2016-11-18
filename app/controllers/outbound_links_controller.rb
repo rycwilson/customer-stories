ff2498a0b1717451338ffd7616b4091a68582775
Ryan Wilson <rycwilson@gmail.com>
2016-11-18 14:17:17 -0800

    removes varmour marketo form

--- app/controllers/outbound_links_controller.rb
+++ app/controllers/outbound_links_controller.rb
@@ -9,10 +9,12 @@ class OutboundLinksController < ApplicationController
     if story.outbound_links.blank?
       story.outbound_links << OutboundLink.create(
                                   url: links[:link1_url],
-                                  link_text: links[:link1_text])
+                                  link_text: links[:link1_text],
+                                  company_id: company.id)
       story.outbound_links << OutboundLink.create(
                                   url: links[:link2_url],
-                                  link_text: links[:link2_text])
+                                  link_text: links[:link2_text],
+                                  company_id: company.id)
     else
       story.outbound_links[0].update(
               url: links[:link1_url],
