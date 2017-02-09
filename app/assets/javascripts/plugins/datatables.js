3d95f62276101979784fcb33a125ccaa358f913b
Ryan Wilson <rycwilson@gmail.com>
2017-02-09 15:21:58 -0800

    adds datetime-moment plugin to datatables

--- app/assets/javascripts/plugins/datatables.js
+++ app/assets/javascripts/plugins/datatables.js
@@ -12,45 +12,45 @@ function initDataTables () {
 
   $('#curate-table').DataTable();
 
-  $('#measure-activity-table').DataTable({
-    lengthMenu: [[-1, 10, 25, 50], ['All', 10, 25, 50]],
-  });
-
+  // Don't specify first column as type: 'date'
+  // with moment.js install, doing so will only screw it up
   $('#story_views-table:not(.short)').DataTable({
     order: [0, 'desc'],
     columns: [
-      { type: 'date' }, null, null, null, null
+      { type: 'datetime-moment' }, null, null, null, null
     ]
   });
   $('#stories_published-table:not(.short)').DataTable({
     order: [0, 'desc'],
     columns: [
-      { type: 'date' }, null, null, null
+      { type: 'datetime-moment' }, null, null, null
     ]
   });
   $('#contributions_submitted-table:not(.short)').DataTable({
     order: [0, 'desc'],
     columns: [
-      { type: 'date' }, null, null, null
+      { type: 'datetime-moment' }, null, null, null
     ]
   });
   $('#contribution_requests_received-table:not(.short)').DataTable({
     order: [0, 'desc'],
     columns: [
-      { type: 'date' }, null, null, null
+      { type: 'datetime-moment' }, null, null, null
     ]
   });
   $('#stories_logo_published-table:not(.short)').DataTable({
     order: [0, 'desc'],
     columns: [
-      { type: 'date' }, null, null, null
+      { type: 'datetime-moment' }, null, null, null
     ]
   });
   $('#stories_created-table:not(.short)').DataTable({
     order: [0, 'desc'],
     columns: [
-      { type: 'date' }, null, null, null
+      { type: 'datetime-moment' }, null, null, null
     ]
   });
+
+
 
 }
--- app/assets/javascripts/plugins/manifest.js
+++ app/assets/javascripts/plugins/manifest.js
@@ -1,4 +1,5 @@
 
+
 // mvp ready
 //= require slimscroll/jquery.slimscroll
 //= require magnific-popup/dist/jquery.magnific-popup
@@ -47,3 +48,61 @@
 
 //= require jstz
 //= require browser_timezone_rails/set_time_zone
+
+
+// datetime-moment plugin
+// https://datatables.net/plug-ins/sorting/datetime-moment
+(function (factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["jquery", "moment", "datatables.net"], factory);
+  } else {
+    factory(jQuery, moment);
+  }
+}(function ($, moment) {
+
+  $.fn.dataTable.moment = function ( format, locale ) {
+    var types = $.fn.dataTable.ext.type;
+
+    // Add type detection
+    types.detect.unshift( function ( d ) {
+      if ( d ) {
+            // Strip HTML tags and newline characters if possible
+            if ( d.replace ) {
+              d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
+            }
+
+            // Strip out surrounding white space
+            d = $.trim( d );
+          }
+
+        // Null and empty values are acceptable
+        if ( d === '' || d === null ) {
+          return 'moment-'+format;
+        }
+
+        return moment( d, format, locale, true ).isValid() ?
+        'moment-'+format :
+        null;
+      } );
+
+    // Add sorting method - use an integer for the sorting
+    types.order[ 'moment-'+format+'-pre' ] = function ( d ) {
+      if ( d ) {
+            // Strip HTML tags and newline characters if possible
+            if ( d.replace ) {
+              d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
+            }
+
+            // Strip out surrounding white space
+            d = $.trim( d );
+          }
+
+          return d === '' || d === null ?
+          -Infinity :
+          parseInt( moment( d, format, locale, true ).format( 'x' ), 10 );
+        };
+      };
+
+    }));
+
+
