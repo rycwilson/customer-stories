2714e99895af2e83cb9b40b05075f6d08271b1ca
Ryan Wilson <rycwilson@gmail.com>
2017-03-10 09:01:33 -0800

    removes saving of tab states into local storage

--- app/assets/javascripts/companies/companies.js
+++ app/assets/javascripts/companies/companies.js
@@ -7,13 +7,5 @@ function attachCompaniesListeners () {
   companiesNewListeners();
   companiesShowListeners();
   companiesEditListeners();
-
-  // remember the last active tab for server submit / page refresh
-  $(document).on('shown.bs.tab', '.mainnav-menu a[data-toggle="tab"]', function () {
-    localStorage.setItem('lastCurateTab', $(this).attr('href'));
-  });
-  $(document).on('shown.bs.tab', '.nav-stacked a[data-toggle="tab"]', function () {
-    localStorage.setItem('lastSettingsTab', $(this).attr('href'));
-  });
 }
 
