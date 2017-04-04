575bf6219e30ddcb4e731155a1debead7b77b7e1
Ryan Wilson <rycwilson@gmail.com>
2017-04-03 18:50:38 -0700

    adds adwords account ids as app constants

--- app/controllers/adwords_controller.rb
+++ app/controllers/adwords_controller.rb
@@ -2,6 +2,13 @@ class AdwordsController < ApplicationController
 
   def index
     respond_to { |format| format.js }
+
+    # get display and re-target campaigns
+    # display the ads
+
   end
 
 end
+
+private
+
