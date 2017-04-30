cb29f5fc5618422ef0ba95d092c1e383c5b5bfc3
Ryan Wilson <rycwilson@gmail.com>
2017-04-30 15:48:54 -0700

    adds s3_delete method to AdwordsImage model

--- app/models/adwords_image.rb
+++ app/models/adwords_image.rb
@@ -4,4 +4,16 @@ class AdwordsImage < ActiveRecord::Base
   has_many :sponsored_stories_images, dependent: :destroy
   has_many :stories, through: :sponsories_images
 
+  before_destroy :s3_delete
+
+  def s3_delete
+    S3_BUCKET.delete_objects(
+      delete: {
+        objects: [
+          key: self.image_url[/.com\/(.+)/, 1]
+        ]
+      }
+    )
+  end
+
 end
