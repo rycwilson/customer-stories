d283b43237902ac79e9d1122f29b6a05cea0cab6
Ryan Wilson <rycwilson@gmail.com>
2017-05-26 17:29:06 -0700

    adds delayed job active record gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509024514) do
+ActiveRecord::Schema.define(version: 20170527002429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,22 @@ ActiveRecord::Schema.define(version: 20170509024514) do
 
   add_index "customers", ["company_id"], name: "index_customers_on_company_id", using: :btree
   add_index "customers", ["name", "company_id"], name: "index_customers_on_name_and_company_id", unique: true, using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "email_contribution_requests", force: :cascade do |t|
     t.integer  "contribution_id"
