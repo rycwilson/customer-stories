4fc664040e985941839dcd1239809f0eea31cf40
Ryan Wilson <rycwilson@gmail.com>
2017-10-26 20:31:43 -0700

    fixes

--- app/assets/javascripts/companies/crowdsource/success_actions.js
+++ app/assets/javascripts/companies/crowdsource/success_actions.js
@@ -1,12 +1,23 @@
 
 function successActionsListeners () {
 
+  var forceFilters = function (successId) {
+    // if the success is not in the result data set after filter is applied,
+    // enable a checkbox filter that will amke it appear
+    // how to choose which filter?
+    // choose the show-published filter, because we want to show all contributors
+    // to a given win/story, not just toggle individual users
+    // but if
+  }
+
   $(document)
 
     .on('click', '.success-actions .manage-contributors', function (e) {
       var successId = $(this).closest('tr').data('success-id');
       $('a[href="#prospect-contributors"]').tab('show');
       $('#contributors-filter').val('success-' + successId).trigger('change');
+      // for a filtered view, default to checkbox filters all applied (nothing hidden)
+      $('.contributors.checkbox-filter input').prop('checked', true).trigger('change');
     })
 
     .on('click', '.success-actions .start-curation', function () {
