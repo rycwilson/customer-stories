ec56e64094ac47732eaac3947aa567d9dfc95981
Ryan Wilson <rycwilson@gmail.com>
2017-11-17 10:47:24 -0800

    bug fix

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -128,7 +128,7 @@ class Contribution < ActiveRecord::Base
         return "opted out&nbsp;&nbsp;<i data-toggle='tooltip' data-placement='top' title='Contributor has opted out of all Customer Stories emails' style='font-size:16px;color:#666' class='fa fa-question-circle-o'></i>".html_safe
       when 'request_re_sent'
         # hack: remind_at holds the re-send date
-        return "request re-sent #{self.request_remind_at.strftime('%-m/%-d/%y')}"
+        return "request re-sent #{self.request_sent_at.strftime('%-m/%-d/%y')}"
     end
   end
 
