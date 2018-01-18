f2db8db532c04ebe2ce8bb3699d88db9aca737f0
Ryan Wilson <rycwilson@gmail.com>
2018-01-17 21:23:37 -0800

    adds csv upload ui and text parsing

--- app/assets/javascripts/companies/1_prospect/new_success.js
+++ app/assets/javascripts/companies/1_prospect/new_success.js
@@ -53,6 +53,63 @@ function newSuccessListeners () {
             $form.find('.create-referrer input[id*="first_name"]')[0].focus();
           }, 0);
         }
+      },
+      isAPIAvailable = function () {
+        // Check for the various File API support.
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+          // Great success! All the File APIs are supported.
+          return true;
+        } else {
+          console.log('browser does not support csv import')
+          // source: File API availability - http://caniuse.com/#feat=fileapi
+          // source: <output> availability - http://html5doctor.com/the-output-element/
+          // document.writeln('The HTML5 APIs used in this form are only available in the following browsers:<br />');
+          // // 6.0 File API & 13.0 <output>
+          // document.writeln(' - Google Chrome: 13.0 or later<br />');
+          // // 3.6 File API & 6.0 <output>
+          // document.writeln(' - Mozilla Firefox: 6.0 or later<br />');
+          // // 10.0 File API & 10.0 <output>
+          // document.writeln(' - Internet Explorer: Not supported (partial support expected in 10.0)<br />');
+          // // ? File API & 5.1 <output>
+          // document.writeln(' - Safari: Not supported<br />');
+          // // ? File API & 9.2 <output>
+          // document.writeln(' - Opera: Not supported');
+          return false;
+        }
+      },
+      printTable = function (file) {
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function (e) {
+          var csv = e.target.result;
+          var data = $.csv.toArrays(csv);
+          // var html = '';
+          // for(var row in data) {
+          //   html += '<tr>\r\n';
+          //   for(var item in data[row]) {
+          //     html += '<td>' + data[row][item] + '</td>\r\n';
+          //   }
+          //   html += '</tr>\r\n';
+          // }
+          // $('#contents').html(html);
+          console.log(data)
+        };
+        reader.onerror = function() { alert('Unable to read ' + file.fileName); };
+      },
+      // ref https://stackoverflow.com/questions/30223361
+      handleFileSelect = function (e) {
+        console.log('handleFileSelect()');
+        var files = $(e.target).find('input[type="file"]')[0].files; // FileList object
+        var file = files[0];
+        // read the file metadata
+        // var output = ''
+        //     output += '<span style="font-weight:bold;">' + escape(file.name) + '</span><br />\n';
+        //     output += ' - FileType: ' + (file.type || 'n/a') + '<br />\n';
+        //     output += ' - FileSize: ' + file.size + ' bytes<br />\n';
+        //     output += ' - LastModified: ' + (file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a') + '<br />\n';
+
+        // read the file contents
+        printTable(file);
       };
 
   $(document)
@@ -164,7 +221,23 @@ function newSuccessListeners () {
       } else {
 
       }
-    });
+    })
+
+    .on('change', '#new-success-form [name="source"]', function () {
+      $('#csv-file-container').toggle();
+      if ($(this).val() === 'import') {
+        // $('#csv-file-input').prop('disabled', false);
+        $('#new-success-form .form-group:not(.source)').addClass('disabled');
+        $('button[type="submit"][form="new-success-form"] span').text('Import CSV File');
+      } else {
+        // $('#csv-file-container').collapse('hide');
+        // $('#csv-file-input').prop('disabled', true);
+        $('#new-success-form .form-group:not(.source)').removeClass('disabled');
+        $('button[type="submit"][form="new-success-form"] span').text('Create Customer Win');
+      }
+    })
+
+    .on('change.bs.fileinput', '#csv-file-container .fileinput', handleFileSelect);
 
     // .on('submit', '#new-success-form', function () {
     //   console.log( $(this).serializeArray() );
