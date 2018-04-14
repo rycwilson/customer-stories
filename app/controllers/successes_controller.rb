6ddf7e975a1e7ca542d27d27bed59634e1bc271a
Ryan Wilson <rycwilson@gmail.com>
2018-04-14 09:56:12 -0700

    adds server-sde validation for zaps/manual entry of successes

--- app/controllers/successes_controller.rb
+++ app/controllers/successes_controller.rb
@@ -28,15 +28,14 @@ class SuccessesController < ApplicationController
 
   def create
     @company = Company.find_by(subdomain: request.subdomain) || current_user.company
-    if params[:zap].present?
-      # use customer name and user emails to find id attributes
-      find_existing_customer_from_zap(params[:success][:customer_attributes])
-      find_existing_users_from_zap(
-        params[:success][:contributions_attributes]['0'][:referrer_attributes],
-        params[:success][:contributions_attributes]['1'][:contributor_attributes]
-      )
-    end
     unless params[:zap].present? && ignore_zap?(params[:success])
+      # use customer name and user emails to find id attributes
+      find_existing_customer(params[:success][:customer_attributes], params[:zap].present?)
+      find_existing_users(
+        params[:success][:contributions_attributes]['0'][:referrer_attributes],
+        params[:success][:contributions_attributes]['1'][:contributor_attributes],
+        params[:zap].present?
+      )
       @success = Success.new(success_params)
       if @success.save
       else
@@ -221,23 +220,29 @@ class SuccessesController < ApplicationController
   # method receives params[:success][:customer_attributes] and either
   # - finds customer, or
   # - provides company_id for the new customer
-  def find_existing_customer_from_zap (customer_params)
-    if (customer = Customer.where(name: customer_params[:name], company_id: current_user.company_id).take)
-      customer_params[:id] = customer.id
-      customer_params.delete_if { |k, v| k != 'id' }
+  def find_existing_customer (customer_params, is_zap)
+    if is_zap || !is_zap  # works for either
+      if (customer = Customer.where(name: customer_params[:name], company_id: current_user.company_id).take)
+        customer_params[:id] = customer.id
+        customer_params.delete_if { |k, v| k != 'id' }
+      else
+        customer_params[:company_id] = current_user.company_id
+      end
     else
-      customer_params[:company_id] = current_user.company_id
     end
   end
 
-  def find_existing_users_from_zap (referrer_params, contributor_params)
-    if (referrer = User.find_by_email(referrer_params[:email]))
-      referrer_params[:id] = referrer.id
-      referrer_params.delete_if { |k, v| !['id', 'title', 'phone'].include?(k) }
-    end
-    if (contributor = User.find_by_email(contributor_params[:email]))
-      contributor_params[:id] = contributor.id
-      contributor_params.delete_if { |k, v| !['id', 'title', 'phone'].include?(k) }
+  def find_existing_users (referrer_params, contributor_params, is_zap)
+    if is_zap || !is_zap  # works for either
+      if (referrer = User.find_by_email(referrer_params[:email]))
+        referrer_params[:id] = referrer.id
+        # allow certain attribute updates
+        referrer_params.delete_if { |k, v| !['id', 'title', 'phone'].include?(k) }
+      end
+      if (contributor = User.find_by_email(contributor_params[:email]))
+        contributor_params[:id] = contributor.id
+        contributor_params.delete_if { |k, v| !['id', 'title', 'phone'].include?(k) }
+      end
     end
   end
 
@@ -305,7 +310,6 @@ class SuccessesController < ApplicationController
       :name
     )
   end
-
 
   def ignore_zap? (success)
     # dup success (name) and customer combination
