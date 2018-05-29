3fdeeb3aaff9c695278eecae664ecab8a144760a
Ryan Wilson <rycwilson@gmail.com>
2018-05-28 22:27:53 -0700

    styling

--- app/assets/javascripts/stories/stories.js
+++ app/assets/javascripts/stories/stories.js
@@ -41,7 +41,7 @@ function storyHoverHandler ($stories) {
       }
       setTimeout(function () {
         $story.find('.caption').children().toggle();
-      }, 200);
+      }, 230);
     },
     function () {
       var $story = $(this);
