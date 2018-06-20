6b4da33cbdd152002293bda8d8062f3a4a32bc5c
Ryan Wilson <rycwilson@gmail.com>
2018-06-19 20:06:05 -0700

    widget stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,9 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+# ref: https://stackoverflow.com/questions/10905905/using-fonts-with-rails-asset-pipeline
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)\z/
+
 Rails.application.config.assets.precompile += %w(
   adwords/ads_preview.js
   jquery-textfill/source/jquery.textfill.js
