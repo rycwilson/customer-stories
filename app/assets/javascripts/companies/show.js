ce0a9a93ffc2534c03a62cd2b86bf97b82c71431
Ryan Wilson <rycwilson@gmail.com>
2018-06-25 16:53:31 -0700

    modifies sign in footer

--- app/assets/javascripts/companies/show.js
+++ app/assets/javascripts/companies/show.js
@@ -19,10 +19,19 @@ function companiesShowListeners () {
     .on('shown.bs.dropdown', '.status.dropdown, .actions.dropdown', function () {
         $(this).closest('tr').addClass('active');
       })
-
     .on('hidden.bs.dropdown', '.status.dropdown, .actions.dropdown', function () {
         $(this).closest('tr').removeClass('active');
-      });
+      })
+    .on('scroll', function () {
+      var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+console.log(scrollBottom)
+      if (scrollBottom < $('#sign-in-footer').height()) {
+        $('#more-stories-container').hide();
+      }
+      else {
+        $('#more-stories-container').show();
+      }
+    });
 
 }
 
