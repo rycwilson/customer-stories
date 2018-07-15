2516dbf17304d63cef81173fc713216c4d4b5386
Ryan Wilson <rycwilson@gmail.com>
2018-07-15 11:11:23 -0700

    adds modal js to bootstrap widget

--- app/assets/javascripts/widgets/gallery/story_overlays.js
+++ app/assets/javascripts/widgets/gallery/story_overlays.js
@@ -1,4 +1,5 @@
 
-//= require 'modernizr/widget_modernizr'
-//= require 'js/classie'
-//= require 'plugins/grid_overlays'
+//= require bootstrap-sass/assets/javascripts/bootstrap/modal
+//= require modernizr/widget_modernizr
+//= require js/classie
+//= require plugins/grid_overlays
