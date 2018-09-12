bd4440bc67256bb8d764362d08eaae7d2e69770e
Ryan Wilson <rycwilson@gmail.com>
2018-09-12 09:45:58 -0700

    fixes to story overlays

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -68,6 +68,7 @@ class StoriesController < ApplicationController
 
   def show
     if params[:is_widget]
+      sleep 5
       # @is_widget = @is_external = true
       respond_to do |format|
         format.js do
