c1cd06889b2b9c5720a993963a60d921c3f63f0e
Ryan Wilson <rycwilson@gmail.com>
2018-09-22 15:02:20 -0700

    adds demo plugin files to precompile manifest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -30,6 +30,9 @@ Rails.application.config.assets.precompile += %w(
   custom/compas/plugins/cs_gallery.css
   custom/compas/plugins/cs_carousel.css
   custom/compas/plugins/cs_tabbed_carousel.css
+  custom/demo/plugins/cs_gallery.css
+  custom/demo/plugins/cs_carousel.css
+  custom/demo/plugins/cs_tabbed_carousel.css
   custom/retailnext/plugins/cs_gallery.css
   custom/retailnext/plugins/cs_carousel.css
   custom/retailnext/plugins/cs_tabbed_carousel.css
