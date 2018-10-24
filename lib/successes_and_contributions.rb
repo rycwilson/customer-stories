f3c3dbacb6e0d2e1e7bf8b475b3afb7463b88c9c
Ryan Wilson <rycwilson@gmail.com>
2018-10-23 19:39:23 -0700

    fixes handling of missing contributors/referrers

--- lib/successes_and_contributions.rb
+++ lib/successes_and_contributions.rb
@@ -3,20 +3,15 @@ module SuccessesAndContributions
   # zaps may have a full name in the first_name field;
   # for multiple words, treat the last one as last name and all others as first name
   def split_full_name (user_params)
-    if user_params[:first_name].split(' ').length > 1
-      user_params[:last_name] = user_params[:first_name].split(' ').pop
-      user_params[:first_name] = user_params[:first_name]
-                                    .split(' ')
-                                    .slice(0, user_params[:first_name].split(' ').length - 1)
-                                    .join(' ')
-    else
-      user_params[:last_name] = 'not provided'
-    end
+    user_params[:last_name] = user_params[:first_name].split(' ').pop
+    user_params[:first_name] = user_params[:first_name]
+                                  .split(' ')
+                                  .slice(0, user_params[:first_name].split(' ').length - 1)
+                                  .join(' ')
     user_params
   end
 
   def find_dup_customer (customer_params, is_zap, current_user)
-    return {} if customer_params.blank?
     if is_zap || !is_zap  # works for either
       if (customer = Customer.where(name: customer_params.try(:[], :name), company_id: current_user.company_id).take)
         # puts existing customer
@@ -31,15 +26,21 @@ module SuccessesAndContributions
     end
   end
 
+  def no_user? (user_params)
+    user_params.blank? ||
+    user_params[:email].blank? ||
+    user_params[:first_name].blank?
+  end
+
   def find_dup_user_and_split_full_name (user_params, is_zap)
     # puts 'find_dup_user_and_split_full_name'
-    return {} if user_params.blank?
+    return {} if no_user?(user_params)
     if is_zap || !is_zap  # works for either
       if (user = User.find_by_email(user_params.try(:[], :email)))
         user_params[:id] = user.id
         user_params.delete_if { |k, v| !['id', 'title', 'phone'].include?(k) }
       else
-        user_params = split_full_name(user_params) if (is_zap && user_params[:last_name].blank?)
+        user_params = split_full_name(user_params) if (is_zap && user_params[:first_name].split(' ').length > 1 && user_params[:last_name].blank?)
         user_params
       end
     end
