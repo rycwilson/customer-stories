ded8a4bc5fdf1359e133199d1c3aef9dccb159b4
Ryan Wilson <rycwilson@gmail.com>
2018-10-31 11:08:19 -0700

    fixes #258

--- app/models/story.rb
+++ app/models/story.rb
@@ -214,7 +214,7 @@ class Story < ApplicationRecord
 
   def scrub_html_input
     white_list_sanitizer = Rails::Html::WhiteListSanitizer.new
-    self.content = white_list_sanitizer.sanitize(content, tags: %w(a p span strong i u blockquote pre font h1 h2 h3 h4 h5 h6 table tr td ol ul li hr img), attributes: %w(id class style face href src))
+    self.content = white_list_sanitizer.sanitize(content, tags: %w(a p span div strong i u em section blockquote cite br pre font h1 h2 h3 h4 h5 h6 table tr td ol ul li hr img), attributes: %w(id class style face href src))
   end
 
   def assign_tags new_story
