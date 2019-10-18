6a13d3572046c67647cb80088427c066fd8e534d
Ryan Wilson <rycwilson@gmail.com>
2019-10-18 10:15:45 -0700

    modifies i18n configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,11 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  
+  # TODO for Rails >= 5.2.2, can use this:
+  # config.i18n.fallbacks = true
+  # else
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
