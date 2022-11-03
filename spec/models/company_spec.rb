0fac4f88757de4da0dadf82bf6b252d6592e0fd7
Ryan Wilson <rycwilson@gmail.com>
2022-11-02 17:07:37 -0700

    adds model spec skeleton code

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Company, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it "is valid with valid attributes"
+    expect(Company.new).to be_valid
   it "is not valid without a name"
-  it "is not valid without a subdomain"
+  it "is not valid without  a subdomain"
   it "is not valid without a website"
 end
