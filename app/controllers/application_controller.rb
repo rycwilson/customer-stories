9a4167ba324e04661b40780725d60bd9456ebe49
Ryan Wilson <rycwilson@gmail.com>
2022-11-18 06:32:11 -0800

    modifies s3 presigned_post call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -254,7 +254,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201')
   end
 
   def company_curator? company_id
