f9c9165db2ff06cdc3705c0c42d04f412d1f5a56
Ryan Wilson <rycwilson@gmail.com>
2022-12-03 05:40:19 -0800

    modifies conditions for s3 delete callback

--- lib/s3_util.rb
+++ lib/s3_util.rb
@@ -1,8 +1,10 @@
 module S3Util
   class << self 
+    # delete from csp-dev-assets bucket OR from .net production environment
+    # do not delete from csp-prod-assets bucket except from .net production environment
     def delete_object(bucket, object_url)
       key = object_url.match(/(amazonaws\.com|cloudfront\.net)\/(.*)/).try(:[], 2)
-      if key
+      if key && (object_url.include?('csp-dev-assets') || ENV['HOST_NAME'].include?('.net'))
         bucket.delete_objects({
           delete: { 
             objects: [{ key: key }] 
