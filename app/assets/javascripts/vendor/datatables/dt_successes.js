3b64a4664339c97abaacb8b152aa8a2ce0c516dc
Ryan Wilson <rycwilson@gmail.com>
2022-12-12 10:25:42 -0800

    replaces success actions underscore template

--- app/assets/javascripts/vendor/datatables/dt_successes.js
+++ app/assets/javascripts/vendor/datatables/dt_successes.js
@@ -1,8 +1,5 @@
-
 function initSuccessesTable (dtSuccessesInit) {
-
-  var successIndex = 1, customerIndex = 2, curatorIndex = 3, statusIndex = 4, storyIndex = 5, actionsIndex = 6;
-
+  const successIndex = 1, customerIndex = 2, curatorIndex = 3, statusIndex = 4, storyIndex = 5, actionsIndex = 6;
   $('#successes-table').DataTable({
     ajax: {
       url: '/successes',
@@ -19,23 +16,20 @@ function initSuccessesTable (dtSuccessesInit) {
     columns: [
       {
         data: null,
-        render: function (data, type, row) {
-          return "<i class='fa fa-caret-right'></i>" +
-                 "<i class='fa fa-caret-down' style='display:none'></i>";
-        }
+        render: (data, type, row) => `
+          <i class="fa fa-caret-right"></i>
+          <i class="fa fa-caret-down" style="display:none"></i>
+        `
       },
       {
         name: 'success',
         data: {
-          _: function (row, type, set, meta) {
-            // console.log(row)
-            return {
-              id: row.id,
-              name: row.name,
-              curatorId: row.curator.id,
-              customerId: row.customer.id
-            };
-          },
+          _: (row, type, set, meta) => ({
+            id: row.id,
+            name: row.name,
+            curatorId: row.curator.id,
+            customerId: row.customer.id
+          }),
           display: 'name',
           filter: 'name',
           sort: 'timestamp' // success.created_at
@@ -52,7 +46,7 @@ function initSuccessesTable (dtSuccessesInit) {
           sort: 'customer.name'
         }
       },
-      {  // curator
+      {
         name: 'curator',
         data: {
           _: 'curator.full_name',
@@ -68,63 +62,50 @@ function initSuccessesTable (dtSuccessesInit) {
       {
         name: 'story',
         data: {
-          _: function (row, type, set, meta) {
-            return row.story && { id: row.story.id, title: row.story.title };
-          }
+          _: (row, type, set, meta) => (
+            row.story && { id: row.story.id, title: row.story.title }
+          )
         },
         defaultContent: 'false'
       },
       {
         data: 'display_status',
-        render: function (data, type, row, meta) {
-            return _.template($('#success-actions-dropdown-template').html())({
-                status: data,
-                story: row.story,
-                storyPath: row.story && '/curate/' + row.customer.slug + '/' + row.story.slug
-              });
-          }
+        render: (data, type, row, meta) => actionsDropdownTemplate(data, row)
       }
     ],
     columnDefs: [
-      { visible: false, targets: [customerIndex, curatorIndex, storyIndex] },
+      { targets: [customerIndex, curatorIndex, storyIndex], visible: false },
       {
         targets: [0, actionsIndex],
         orderable: false,
         searchable: false,
-        createdCell: function (td, cellData, rowData, row, col) {
-          if (col === 0) {
-            $(td).addClass('toggle-success-child');
-          } else {
-            $(td).addClass('actions dropdown');
-          }
-        }
+        createdCell: (td, cellData, rowData, row, col) => (
+          $(td).addClass(col === 0 ? 'toggle-success-child' : 'actions dropdown')
+        )
       },
-      { width: '0%', targets: [customerIndex, curatorIndex, storyIndex] },  // hidden
-      { width: '5%', targets: 0 },
-      { width: '61%', targets: successIndex },
-      { width: '26%', targets: statusIndex },
-      { width: '8%', targets: actionsIndex }
+      { targets: [customerIndex, curatorIndex, storyIndex],  width: '0%' },  // hidden
+      { targets: 0, width: '5%' },
+      { targets: successIndex, width: '61%' },
+      { targets: statusIndex, width: '26%' },
+      { targets: actionsIndex, width: '8%' }
     ],
     rowGroup: {
       dataSrc: 'customer.name',
       startRender: function (groupRows, successName) {
         // console.log($(this))   //  [RowGroup]
-        var customerId = $('#successes-table').DataTable()
-                           .rows( groupRows[0][0] )
-                           .data()[0]
-                           .customer.id;
-        return $('<tr/>').append(
-          '<td colspan="3">' +
-             '<span style="font-weight:600">' +
-                groupRows.data()[0].customer.name +
-             '</span>' +
-          '</td>' +
-          '<td colspan="1">' +
-             '<button data-toggle="modal" data-target="#edit-customer-modal" data-customer-id="' + customerId + '">' +
-                '<i class="glyphicon glyphicon-pencil"></i>' +
-             '</button>' +
-          '</td>'
-        );
+        const customerId = $('#successes-table').DataTable().rows(groupRows[0][0]).data()[0].customer.id;
+        return $('<tr/>').append(`
+          <td colspan="3">
+            <span style="font-weight:600">
+              ${groupRows.data()[0].customer.name}
+            </span>
+          </td>
+          <td colspan="1">
+            <button data-toggle="modal" data-target="#edit-customer-modal" data-customer-id="${customerId}">
+              <i class="glyphicon glyphicon-pencil"></i>
+            </button>
+          </td>
+        `);
       }
     },
     createdRow: function (row, data, index) {
@@ -136,8 +117,7 @@ function initSuccessesTable (dtSuccessesInit) {
       $(row).children().eq(3).addClass('actions dropdown');
     },
     initComplete: function (settings, json) {
-      var $table = $(this), dt = $table.DataTable();
-          $tableWrapper = $table.closest('[id*="table_wrapper"]');
+      const $tableWrapper = $(this).closest('[id*="table_wrapper"]');
       
       // remove default search field.  Disabling via options also disables api, so can't do that
       $tableWrapper.children('.row:first-child').remove();
@@ -155,6 +135,76 @@ function initSuccessesTable (dtSuccessesInit) {
       //                .appendTo($tableWrapper.find('.select-filters'));
       // });
     }
-
   });
+
+  function actionsDropdownTemplate(displayStatus, rowData) {
+    const noContributorsAdded = displayStatus.match(/0.+Contributors\sadded/);
+    const noContributorsInvited = displayStatus.match(/0.+Contributors\sinvited/);
+    const contributionsExist = displayStatus.match(/[^0]&nbsp;&nbsp;Contributions\ssubmitted/);
+    const storyExists = rowData.story;
+    const storyPath = storyExists && `/curate/${rowData.customer.slug}/${rowData.story.slug}`;
+    const contributorsAction = (() => {
+      const className = `${noContributorsAdded ? 'add' : 'manage'}-contributors`;
+      const action = noContributorsAdded ? 'Add' : (noContributorsInvited ? 'Invite' : 'Manage');
+      return `
+        <li class="${className}">
+          <a href="javascript:;">
+            <i class="fa fa-users fa-fw action"></i>&nbsp;&nbsp;
+            <span>${action} Contributors</span>
+          </a>
+        </li>
+      `.trim();
+    })();
+    return `
+      <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="fa fa-caret-down"></i>
+      </a>
+      <ul class="success-actions dropdown-menu dropdown-menu-right">
+        ${contributionsExist ? `
+            <li class="view-submissions">
+              <a href="javascript:;">
+                <i class="fa fa-comments fa-fw action"></i>&nbsp;&nbsp;
+                <span>View Contributions</span>
+              </a>
+            </li>
+            <li role="separator" class="divider"></li>
+          ` : 
+          ''
+        }
+        ${storyExists ? 
+            [['story-settings', 'fa-gear'], ['story-content', 'fa-edit'], ['story-contributors', 'fa-users']]
+              .map(([className, icon]) => {
+                const section = (
+                  className[className.indexOf('-') + 1].toUpperCase() + 
+                  className.slice(className.indexOf('-') + 2, className.length)
+                )
+                return `
+                  <li class="${className}">
+                    <a href="${storyPath}">
+                      <i class="fa ${icon} fa-fw action"></i>&nbsp;&nbsp;
+                      <span>Customer Story ${section}</span>
+                    </a>
+                  </li>
+                `.trim();
+              }).join('') : `
+            ${contributorsAction}
+            <li role="separator" class="divider"></li>
+            <li class="start-curation">
+              <a href="javascript:;">
+                <i class="fa fa-play fa-fw action"></i>&nbsp;&nbsp;
+                <span>Start Customer Story</span>
+              </a>
+            </li>
+          `
+        }
+        <li role="separator" class="divider"></li>
+        <li class="remove">
+          <a href="javascript:;">
+            <i class="fa fa-remove fa-fw action"></i>&nbsp;&nbsp;
+            <span>Remove</span>
+          </a>
+        </li>
+      </ul>
+    `.trim();
+  }
 }
