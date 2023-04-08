f1f93c2a33a65affc6a485a83bc347e85249d3fd
Ryan Wilson <rycwilson@gmail.com>
2023-04-07 17:10:52 -0700

    adds collapsible header behavior

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -5,3 +5,51 @@
 //= require mvpready-helpers
 //= require mvpready-landing
 //= require bootstrap-validator/dist/validator
+
+let resizeTimer;
+
+setViewportHeight();
+collapseHeaderOnScroll();
+
+addEventListener('resize', (e) => {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(setViewportHeight, 500);
+})
+
+// https://css-tricks.com/snippets/jquery/done-resizing-event/
+function setViewportHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
+function collapseHeaderOnScroll() {
+  const navbar = document.querySelector('.navbar-fixed-top');
+  const minScroll = 10;
+  let isScrolling = false;
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', onScroll, { passive: true });
+  
+  function toggleCollapse() {
+    const scrollTop = window.scrollY;
+    if (Math.abs(lastScrollTop - scrollTop) <= minScroll) return false;
+    if (scrollTop > lastScrollTop && scrollTop > navbar.offsetHeight) {
+      // navbar.classList.add('collapse');
+      navbar.style.top = '-85px'
+    } else {
+      // navbar.classList.remove('collapse');
+      navbar.style.top = '0px'
+    }
+    lastScrollTop = scrollTop;
+  }
+
+  function onScroll() {
+    if (isScrolling) return false;
+    isScrolling = true;
+    setTimeout(() => {
+      toggleCollapse();
+      isScrolling = false;
+    }, 250);
+  }
+}
