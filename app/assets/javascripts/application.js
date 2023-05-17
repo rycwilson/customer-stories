f1b1106e8de325f43f7232a97d307cebf4f93a6d
Ryan Wilson <rycwilson@gmail.com>
2023-05-17 12:47:17 -0700

    adds refactors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,3 @@
-
 /* jquery-ui must appear before bootstrap, per https://stackoverflow.com/questions/13731400 */
 //= require jquery3
 //= require jquery_ujs
@@ -35,10 +34,10 @@ $(document).on('turbolinks:load', function (e) {
 });
 
 function setAppData () {
-  if (window.gon) {  // use window.gon in case undefined
-    // console.log('gon: ', gon);
+  if (window.gon) {  // use global namespace in case gon is undefined
+    console.log('gon: ', gon);
   } else {
-    // console.log('gon undefined');
+    console.log('gon undefined');
   }
   CSP.company = (window.gon && gon.company) || CSP.company || null;
   CSP.stories = (window.gon && gon.stories) || CSP.stories || null;
@@ -53,30 +52,19 @@ function attachAppListeners () {
 
   xScrollBoundaries();
 
+  window.onpopstate = showActiveTabContent;
+
   $(document)
 
-    .on('click', '.nav-workflow a', function (e) {
-      e.preventDefault();
-      var currentWorkflowPath = window.location.pathname,
-          newWorkflowPath = '/' + $(this).attr('href').slice(1, $(this).attr('href').length);
+    .on('click', '.nav-workflow a', workflowTurboVisit)
 
-      if ($('body').hasClass('companies show')) {
-        // replacing state ensure turbolinks:false for the first tab state
-        window.history.replaceState({ turbolinks: false }, null, currentWorkflowPath);
-        window.history.pushState({ turbolinks: true }, null, newWorkflowPath);
-
-      } else {
-        Turbolinks.visit(newWorkflowPath);
-      }
-    })
     // apply styling when clicking on a company nav dropdown option,
-    .on('click', 'a[href*="/settings"], a[href="/user-profile"]',
-      function () {
-        var $thisDropdown = $(this).closest('li.dropdown'),
-            $otherDropdown = $thisDropdown.parent().find('li.dropdown:not(.open)');
-        $thisDropdown.addClass('active');
-        $otherDropdown.removeClass('active');
-      })
+    .on('click', 'a[href*="/settings"], a[href="/user-profile"]', (e) => {
+      const thisDropdown = e.target.closest('li.dropdown');
+      const otherDropdown = thisDropdown.nextElementSibling || thisDropdown.previousElementSibling;
+      thisDropdown.classList.add('active');
+      otherDropdown.classList.remove('active');
+    })
 
     /**
      * some forms excluded as they have their own handler, e.g. #new-success-form, #gads-form
@@ -112,37 +100,7 @@ function attachAppListeners () {
     });
 
   //
-  window.onpopstate = function (e) {
-    var workflowMatch = window.location.pathname.match(
-            /(prospect|curate|promote|measure)(\/(\w|-)+)?/
-          ),
-        workflowStage = workflowMatch && workflowMatch[1],
-        curateView = workflowStage && (workflowStage === 'curate') ?
-                      (workflowMatch[2] ? 'story' : 'stories') : 
-                      null;
-
-    if (workflowStage) {
-      $('.nav-workflow a[href="#' + workflowStage + '"]').tab('show');
-      if (curateView) {
-        if (curateView === 'stories') {
-          $('a[href=".curate-stories"]').tab('show'); 
-        } else {
-          $('a[href=".edit-story"]').tab('show');
-        }
-        
-        // don't scroll to panel
-        setTimeout(function() { window.scrollTo(0, 0); }, 1);
-        if (curateView === 'stories') {
-          $('#curate-filters .curator')
-            .val(
-              $('#curate-filters .curator').children('[value="' + CSP.current_user.id + '"]').val()
-            )
-            .trigger('change', { auto: true });
-        }
-      }
-    }
-
-  };
+  
 
   // window.onorientationchange = function () { window.location.reload(); };
 
@@ -190,19 +148,38 @@ function attachAppListeners () {
     });
 }
 
+function workflowTurboVisit(e) {
+  e.preventDefault();
+  const link = e.currentTarget;
+  const newWorkflowPath = `/${link.getAttribute('href').slice(1, link.getAttribute('href').length)}`;
+  const isDashboard = document.body.classList.contains('companies') && document.body.classList.contains('show');
+  if (isDashboard) {
+    // replacing state ensures turbolinks:false for the first tab state
+    history.replaceState({ turbolinks: false }, null, location.pathname);
+    history.pushState({ turbolinks: true }, null, newWorkflowPath);
+  } else {
+    Turbolinks.visit(newWorkflowPath);
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function showActiveTabContent(e) {
+  const workflowMatch = location.pathname.match(/(prospect|curate|promote|measure)(\/(\w|-)+)?/);
+  const workflowStage = workflowMatch && workflowMatch[1];
+  const curateView = workflowStage && (workflowStage === 'curate') ? (workflowMatch[2] ? 'story' : 'stories') : null;
+  if (workflowStage) {
+    $(`.nav-workflow a[href="#${workflowStage}"]`).tab('show');
+    if (curateView) {
+      curateView === 'stories' ? 
+        $('a[href=".curate-stories"]').tab('show') :
+        $('a[href=".edit-story"]').tab('show');
+      
+      // don't scroll to panel
+      setTimeout(() => scrollTo(0, 0));
+      if (curateView === 'stories') {
+        $('#curate-filters .curator')
+          .val($('#curate-filters .curator').children(`[value="${CSP.current_user.id}"]`).val())
+          .trigger('change', { auto: true });
+      }
+    }
+  }
+}
