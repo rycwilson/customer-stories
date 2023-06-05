bed9ecf7522d12a09e769fbce16b95f14423ec52
Ryan Wilson <rycwilson@gmail.com>
2023-06-05 11:37:11 -0700

    modifies rails setting config.assets.debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -89,7 +89,10 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  # config.assets.debug = true
+
+  # https://github.com/rails/jsbundling-rails/issues/40#issuecomment-1006503192
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
