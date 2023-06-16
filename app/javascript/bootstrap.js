0c115f310d47a576b777e4f8699598e06ad3f690
Ryan Wilson <rycwilson@gmail.com>
2023-06-16 15:18:04 -0700

    adds bootstrap modal to js bundle

--- app/javascript/bootstrap.js
+++ app/javascript/bootstrap.js
@@ -4,5 +4,6 @@ import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/tab.js';
 import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/dropdown.js';
 import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/button.js';
 import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/tooltip.js';
+import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/modal.js';
 
 import 'jquery3-bootstrap-switch/dist/js/bootstrap-switch.js';
