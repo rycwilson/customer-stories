7b7051f947b12d28e50b68f091d31c2d5fe03d66
Ryan Wilson <rycwilson@gmail.com>
2023-07-10 16:53:29 -0700

    adds behavior to modal controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,32 +1,78 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['title', 'turboFrame', 'railsForm', 'footer'];
+  static targets = ['title', 'body', 'turboFrame', 'form', 'dismissBtn', 'submitBtn'];
   static values = { 
+    title: { type: String, default: 'title is missing' },
     turboFrameAttrs: { type: Object, default: {} },
-    newActionClassName: String, 
-    showActionClassName: String, 
-    editActionClassName: String 
+    bodyContent: { type: String, default: '' },
   };
 
+  static hiddenHandler;
+  static ajaxSuccessHandler;
+  // static beforeFrameRenderHandler;
+  formId;
+  
+  initialize() {
+    this.hiddenHandler = this.onHidden.bind(this);
+    this.ajaxSuccessHandler = this.onAjaxSuccess.bind(this);
+    // this.beforeFrameRenderHandler = this.beforeFrameRender.bind(this);
+  }
+
   connect() {
+    $(this.element).on('hidden.bs.modal', this.hiddenHandler);
   }
 
   disconnect() {
+    $(this.element).off('hidden.bs.modal', this.hiddenHandler);
   }
 
-  turboFrameAttrsValueChanged(attrs) {
-    if (Object.keys(attrs).includes('id') && Object.keys(attrs).includes('src')) {
-      this.turboFrameTarget.id = attrs.id;
-      this.turboFrameTarget.src = attrs.src;
-      [this.newActionClassNameValue, this.showActionClassNameValue, this.editActionClassNameValue]
-        .forEach(className => {
-          const action = className.match(/--(?<action>\w+)/).groups.action;
-          this.footerTarget.classList.toggle(className, this.isAction(action));
-        });
+  titleValueChanged(newTitle) {
+    this.titleTarget.textContent = newTitle;
+  }
+
+  bodyContentValueChanged(newContent, oldContent) {
+    [...this.bodyTarget.children].forEach(child => { if (!child.isSameNode(this.turboFrameTarget)) child.remove(); });
+    if (newContent) {
+      this.turboFrameTarget.classList.add('hidden');
+      this.bodyTarget.insertAdjacentHTML('beforeend', newContent);
+    } else {
+      this.turboFrameTarget.classList.remove('hidden');
     }
   }
 
+  turboFrameAttrsValueChanged(attrs) {
+    if (attrs.id && attrs.src) {
+      if (attrs.id.match(/^(new|edit)/)) this.actionValue = attrs.id.match(/^(?<action>new|edit)/).groups.action;
+      this.turboFrameTarget.insertAdjacentHTML('afterbegin', this.turboFrameTarget.dataset.placeholder);
+      this.turboFrameTarget.id = attrs.id;
+      this.turboFrameTarget.src = attrs.src;
+    } else {
+      this.turboFrameTarget.replaceChildren();
+    }
+  }
+
+  onFrameRender(e) {
+    console.log('turbo:before-frame-render', e.detail.newFrame);
+    if (this.hasFormTarget) {
+      this.dismissBtnTarget.textContent = 'Cancel';
+      this.submitBtnTarget.value = this.formTarget.dataset.submitBtnText;
+      this.submitBtnTarget.classList.remove('hidden');
+      this.submitBtnTarget.setAttribute('form', this.formTarget.id);
+      $(this.formTarget).on('ajax:success', this.ajaxSuccessHandler);
+    } else {
+      this.dismissBtnTarget.textContent = 'Close';
+    }
+  }
+  
+  onAjaxSuccess(e, data, status, xhr) {
+    if (data.status === 'ok') {
+      this.hide();
+    } else {
+      // handle errors
+    }
+  }
+  
   show() {
     $(this.element).modal('show');
   }
@@ -35,25 +81,15 @@ export default class extends Controller {
     $(this.element).modal('hide');
   }
 
-  onSuccess(e, data, status, xhr) {
-    if (data.status === 'ok') {
-      this.hide();
-    } else {
-      // handle errors
-    }
-  }
-  
-  onFrameRender(e) {
-    if (this.hasRailsFormTarget) {
-      $(this.railsFormTarget).on('ajax:success', this.onSuccess.bind(this))
-    }
+  changeSubmitBtnText({ detail: { submitBtnText } }) {
+    this.submitBtnTarget.value = submitBtnText;
   }
 
-  isAction(action) {
-    if (!this.turboFrameAttrsValue.src) return false;
-    if (action === 'new') return this.turboFrameAttrsValue.src.match(/\/\w+$/)
-    else if (action === 'show') return this.turboFrameAttrsValue.src.match(/\/\d+$/)
-    else if (action === 'edit') return this.turboFrameAttrsValue.src.match(/\/edit$/)
-    else return false;
+  onHidden() {
+    if (this.hasFormTarget) $(this.formTarget).off('ajax:success', this.ajaxSuccessHandler);
+    this.turboFrameAttrsValue = {};
+    this.bodyContentValue = '';
+    this.dismissBtnTarget.textContent = '';
+    this.submitBtnTarget.value = '';
   }
 }
