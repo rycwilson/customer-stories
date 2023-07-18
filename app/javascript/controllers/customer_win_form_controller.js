e922db2979e2fd2762f92ebd625e8f765f2eb4d1
Ryan Wilson <rycwilson@gmail.com>
2023-07-18 10:43:22 -0700

    adds more new customer win behavior

--- app/javascript/controllers/customer_win_form_controller.js
+++ app/javascript/controllers/customer_win_form_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['customerSelect'];
+  static targets = ['referrerSelect', 'referrerFields', 'contributorSelect', 'contributorFields'];
 
-  onCustomerChange(e) {
-
+  onContactChange({ target: select }) {
+    const isNewContact = select.value === '0';
+    const contactFields = select.isSameNode(this.contributorSelectTarget) ? 
+      this.contributorFieldsTarget : 
+      this.referrerFieldsTarget;
+    contactFields.setAttribute('data-new-contact-should-enable-value', isNewContact)
   }
 }
--- app/javascript/controllers/new_contact_controller.js
+++ app/javascript/controllers/new_contact_controller.js
@@ -1,17 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static outlets = ['contribution-form'];
+  static outlets = ['customer-win-form', 'contribution-form'];
   static targets = ['contactField', 'firstName'];
   static values = { 
     shouldEnable: { type: Boolean, default: false },
     parentSelectTarget: String
   };
 
+  formController;
+
   shouldEnableValueChanged(shouldEnable, wasEnabled) {
     if (wasEnabled === undefined) return false;
     this.element.classList.toggle('hidden', !shouldEnable);
-    this.contributionFormOutlet[this.parentSelectTargetValue].disabled = shouldEnable;
+    this.formCtrl()[this.parentSelectTargetValue].disabled = shouldEnable;
     this.contactFieldTargets.forEach(input => {
       input.disabled = !shouldEnable;
       if (!shouldEnable && input.type !== 'hidden') input.value = '';
@@ -21,5 +23,14 @@ export default class extends Controller {
 
     });
     if (shouldEnable) this.firstNameTarget.focus();
+  }
+
+  // parent form controller
+  formCtrl() {
+    this.formController = this.formController || (
+      (this.element.hasAttribute(`data-${this.identifier}-customer-win-form-outlet`) && this.customerWinFormOutlet) ||
+      (this.element.hasAttribute(`data-${this.identifier}-contribution-form-outlet`) && this.contributionFormOutlet)
+    );
+    return this.formController;
   }
 }
