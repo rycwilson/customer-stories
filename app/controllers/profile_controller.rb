22cb227c31f2de1b1afdf72b5b3321f22e116522
Ryan Wilson <rycwilson@gmail.com>
2023-08-02 10:37:47 -0700

    modifies user-switching to avoid email addresses in source code

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,20 +1,16 @@
 class ProfileController < ApplicationController
 
   SWITCH_USERS = [
-    'dan@customerstories.net', 
-    'dlindblo@gmail.com', 
-    'demo@customerstories.net',
-    'acme-test@customerstories.net',
-    'ryanbpalo@customerstories.com',
-    'rycwilson@gmail.com', 
-    'heather@trunity.com', 
-    'haley@pixleeteam.com',
-    'awad@pixleeteam.com',
-    'colin@pixleeteam.com',
-    'bill@c4ce.com',
-    'carlos@compas.global',
-    'kturner@varmour.com',
-    'alex.salai@retailnext.net'
+    'Dan acme-test',
+    'Dan Lindblom',
+    'Dan Demo',
+    'Ryan Wilson',
+    'Ryan Palo',
+    'Bill Lee',
+    'Carlos Ramon',
+    'Kevin Turner',
+    'Haley Fraser',
+    'Rachelle Benson'
   ]
 
   def switch
@@ -33,12 +29,10 @@ class ProfileController < ApplicationController
     @is_admin = current_user.admin? || @original_user&.admin?
     set_s3_direct_post()
     if @is_admin
-      @switch_users = User.where(email: SWITCH_USERS).map do |user|
-        { 
-          id: user.id, 
-          email: user.email,
-          name: "#{user.full_name}" + (user.company.present? ? "\s(#{user.company.name})" : '') 
-        }
+      @switch_users = SWITCH_USERS.flat_map do |name|
+        User.where.not(company_id: nil)
+          .where(first_name: name.split(' ').first, last_name: name.split(' ').last)
+          .map { |user| { id: user.id, email: user.email, name: "#{user.full_name} (#{user.company.name})" }}
       end
     end
   end 
