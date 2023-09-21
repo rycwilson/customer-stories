49087146d994a02d465bfed7000f4f0a771f77ff
Ryan Wilson <rycwilson@gmail.com>
2023-09-21 16:54:51 -0700

    fixes subdomain validator to include correct max length

--- lib/subdomain_validator.rb
+++ lib/subdomain_validator.rb
@@ -1,6 +1,6 @@
 class SubdomainValidator < ActiveModel::Validator
   def validate(record)
-    required_pattern = /\A[a-z0-9]{3,32}\z/
+    required_pattern = /\A[a-z0-9]{3,63}\z/
     excluded_patterns = /\A(www[0-9]*|mail[0-9]*|smtp[0-9]*|ftp[0-9]*|pop[0-9]*|imap[0-9]*|ns[0-9]*)\z/
 
     unless record.subdomain =~ required_pattern
