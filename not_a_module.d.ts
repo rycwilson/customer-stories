95b3ba66df974de80443e0df37569b5d361ab2c9
Ryan Wilson <rycwilson@gmail.com>
2023-09-26 19:55:11 -0700

    adds datatables module declaration

--- not_a_module.d.ts
+++ not_a_module.d.ts
@@ -2,3 +2,4 @@ declare module 'tom-select/dist/esm/tom-select.js'
 declare module 'tom-select/dist/js/plugins/clear_button'
 
 declare module 'jquery/dist/jquery.js'
+declare module 'datatables.net-bs/js/dataTables.bootstrap.js';
