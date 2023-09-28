a50a18f77176957c7a05419b47322354474a88ef
Ryan Wilson <rycwilson@gmail.com>
2023-09-28 11:26:47 -0700

    adds type annotations

--- app/javascript/util.ts
+++ app/javascript/util.ts
@@ -1,46 +1,54 @@
-export async function getJSON(dataPath) {
-  const headers = {
-    'Content-Type': 'application/json', 
-    'X-CSRF-Token': document.querySelector('[name="csrf-token" ]').content
-  };
-  try {
-    // return await Promise.all([
-    //   fetch('/successes', headers).then(res => res.json()), 
-    //   fetch('/companies/0/contributions', headers).then(res => res.json())
-    // ]);
-    return await fetch(`${dataPath}.json`, headers).then(res => res.json());
-  } catch(err) {
-    console.error(err);
+export async function getJSON(dataPath: string) {
+  const csrfTokenMeta = document.querySelector<HTMLMetaElement>('[name="csrf-token" ]');
+  const csrfToken = csrfTokenMeta?.content;
+  if (csrfToken) {
+    const options: RequestInit = {
+      headers: {
+        'Content-Type': 'application/json', 
+        'X-CSRF-Token': csrfToken
+      }
+    };
+    try {
+      // return await Promise.all([
+      //   fetch('/successes', headers).then(res => res.json()), 
+      //   fetch('/companies/0/contributions', headers).then(res => res.json())
+      // ]);
+      return await fetch(`${dataPath}.json`, options).then(res => res.json());
+    } catch(err) {
+      console.error(err);
+    }
+  } else {
+    console.error(`No CSRF token found for fetch(${dataPath}.json)`)
   }
 }
 
-export function capitalize(word) {
+export function capitalize(word: string) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-export function kebabToCamel(str) {
+export function kebabToCamel(str: string) {
   return str.replace(/-./g, (char) => char[1].toUpperCase());
 }
 
-export function kebabize(str) {
+export function kebabize(str: string) {
   return str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase());
 }
 
-export function copyToClipboard(str) {
-  const onCopy = (e) => {
-    e.clipboardData.setData("text/html", str);
-    e.clipboardData.setData("text/plain", str);
-    e.preventDefault();
-  };
-  document.addEventListener("copy", onCopy);
-  document.execCommand("copy");
-  document.removeEventListener("copy", onCopy);
-}
+// export function copyToClipboard(str) {
+//   const onCopy = (e) => {
+//     e.clipboardData.setData("text/html", str);
+//     e.clipboardData.setData("text/plain", str);
+//     e.preventDefault();
+//   };
+//   document.addEventListener("copy", onCopy);
+//   document.execCommand("copy");
+//   document.removeEventListener("copy", onCopy);
+// }
 
-export function distinctItems(items) {
+export function distinctItems(items: string[] | number[]) {
   return items.filter((item, i, _items) => i === _items.indexOf(item));
 }
 
-export function distinctObjects(objects, attr) {
+export function distinctObjects(objects: { [i: string]: any }[], attr: string) {
   return objects.filter((obj, i, _objects) => i === _objects.findIndex(_obj => _obj[attr] === obj[attr]));
 }
