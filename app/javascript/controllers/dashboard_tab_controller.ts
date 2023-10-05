e12dbf50982145c87ad8d87862f95a30a429bf56
Ryan Wilson <rycwilson@gmail.com>
2023-10-05 13:49:36 -0700

    adds type annotations to dashboard tab controller

--- app/javascript/controllers/dashboard_tab_controller.ts
+++ app/javascript/controllers/dashboard_tab_controller.ts
@@ -1,19 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
+import Turbo from "@hotwired/turbo"
 
+type DashboardPath = '/prospect' | '/curate' | '/promote' | '/measure';
 export default class extends Controller<HTMLAnchorElement> {
-  onClick(e) {
+  onClick(e: Event) {
     e.preventDefault();
-    const newDashboardPath = `/${this.element.getAttribute('href').slice(1, this.element.getAttribute('href').length)}`;
+    const href = this.element.getAttribute('href');
+    const newDashboardPath = href ? `/${href.slice(1, href.length)}` : '';
     const currentlyOnDashboard = this.element.dataset.dashboardTarget === 'tab';
     if (currentlyOnDashboard) {
       // replacing state ensures turbo:false for the first tab state
-      history.replaceState({ turbo: false }, null, location.pathname);
+      history.replaceState({ turbo: false }, '', location.pathname);
       history.pushState(
         { turbo: { restorationIdentifier: Turbo.navigator.history.restorationIdentifier } }, 
-        null, 
+        '', 
         newDashboardPath
       );
-    } else {
+    } else if (newDashboardPath) {
       // const dropdowns = document.querySelectorAll('#company-nav .nav-settings > li.dropdown');
       // dropdowns.forEach(dropdown => dropdown.classList.remove('active'));
       Turbo.visit(newDashboardPath);
