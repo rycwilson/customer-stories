409099bb81bdc758475336f0f823aa6bbfd5ef78
Ryan Wilson <rycwilson@gmail.com>
2023-10-05 13:47:23 -0700

    removes module declarations

--- not_a_module.d.ts
+++ not_a_module.d.ts
@@ -1,6 +1,7 @@
-declare module 'tom-select/dist/esm/tom-select'
+// declare module 'tom-select/dist/esm/tom-select'
 declare module 'tom-select/dist/js/plugins/clear_button'
 
-declare module 'jquery/dist/jquery'
-// declare module 'datatables.net-bs/js/dataTables.bootstrap';
+// declare module 'jquery/dist/jquery'
 
+// declare module 'datatables.net-bs';
+// declare module 'datatables.net-rowgroup-bs';
