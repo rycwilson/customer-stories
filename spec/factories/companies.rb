b8be9a61bfc48201af3f50b74d8e7299b6499778
Ryan Wilson <rycwilson@gmail.com>
2023-10-25 17:59:35 -0700

    adds Product factory and model spec

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
-  factory :company do
+  # the class argument can be left out and will be inferred from the factory name
+  factory :company, class: 'Company' do
     name { Faker::Company.name }
     subdomain { Faker::Internet.domain_word }
     website { 'https://acme.com' }
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.build(:product) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do 
+    subject.name = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a unique name within the scope of a company' do 
+    company = FactoryBot.create(:company)
+    FactoryBot.create(:product, name: 'Example Product', company: company)
+    subject.name = 'Example Product'
+    subject.company = company
+    expect(subject).not_to be_valid
+  end
 end
