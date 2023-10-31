781c3d2816d1ebcd9c292db972704fc740c5be8b
Ryan Wilson <rycwilson@gmail.com>
2023-10-31 11:18:25 -0700

    adds User factory and spec

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    sign_up_code { "csp_beta" }
+    skip_callbacks { true }
+
+    trait :with_company do
+      association :company
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:company).optional }
+    
+    context 'when user is a curator' do
+      subject(:user) { build(:user, :with_company) }
+
+      it { is_expected.to have_many(:successes).with_foreign_key('curator_id') }
+    end
+
+
+    # it { is_expected.to have_many(:stories).through(:successes) }
+    # it { is_expected.to have_many(:contributions).through(:successes) }
+
+    # TODO test that the contributors association is distinct (users can have multiple contributions)
+    # it { is_expected.to have_many(:contributors).through(:contributions).source(:contributor) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    # TODO: phone format validation
+  end
+
+  describe 'factory' do
+    it { is_expected.to be_valid }
+
+    context 'when user is a curator' do
+      subject(:user) { build(:user, :with_company) }
+
+      it 'belongs to a company' do
+        expect(user.company).to be_present
+      end
+    end
+  end
+
+  describe '#full_name' do
+    it 'cocatenates first and last names' do
+      expect(user.full_name).to eq(user.first_name + " " + user.last_name)
+    end
+  end
 end
