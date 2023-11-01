93f63ae31e45230fcf7c09002fcd5f707484f89c
Ryan Wilson <rycwilson@gmail.com>
2023-11-01 12:21:24 -0700

    adds select targets to stories controller

--- app/typescript/src/controllers/stories_controller.ts
+++ app/typescript/src/controllers/stories_controller.ts
@@ -2,11 +2,26 @@ import { Controller } from '@hotwired/stimulus';
 import imagesLoaded from 'imagesloaded';
 
 export default class extends Controller<HTMLDivElement> {
-  static targets = ['searchAndFilters', 'filterSelect', 'gallery', 'card'];
+  static targets = [
+    'searchAndFilters',   // one container for xs and sm, another for md and lg
+    'filterSelect', 
+    'gallery', 
+    'card', 
+    'curatorSelect', 
+    'statusSelect', 
+    'customerSelect',
+    'categorySelect',
+    'productSelect'
+  ];
   declare readonly searchAndFiltersTargets: HTMLDivElement[];
   declare readonly filterSelectTargets: HTMLSelectElement[];
   declare readonly galleryTarget: HTMLDivElement;
   declare readonly cardTargets: HTMLDivElement[];
+  declare readonly curatorSelectTarget: HTMLSelectElement;
+  declare readonly statusSelectTarget: HTMLSelectElement;
+  declare readonly customerSelectTarget: HTMLSelectElement;
+  declare readonly categorySelectTarget: HTMLSelectElement;
+  declare readonly productSelectTarget: HTMLSelectElement;
   
   readyFilters = 0;
 
@@ -16,10 +31,13 @@ export default class extends Controller<HTMLDivElement> {
       // console.log('images loaded', e)
       this.galleryTarget.classList.remove('hidden');
     })
+
+    console.log('filterSelect: ', this.filterSelectTargets)
   }
 
   onInitFilter(e: Event) {
-    if (++this.readyFilters === this.filterSelectTargets.length) 
+    if (++this.readyFilters === this.filterSelectTargets.length) {
       this.searchAndFiltersTargets.forEach(container => container.setAttribute('data-init', 'true'));
+    }
   }
 }
