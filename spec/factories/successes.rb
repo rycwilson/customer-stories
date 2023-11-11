1de7f8e6d26fa8052b4da599aa61e2d6017c7c18
Ryan Wilson <rycwilson@gmail.com>
2023-11-11 10:10:52 -0800

    adds associations to Success factory

--- spec/factories/successes.rb
+++ spec/factories/successes.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :success do
     name { Faker::Lorem.sentence }
-    company 
     customer 
+    company
     association :curator, factory: :user
 
     factory :success_with_contributions do
@@ -10,8 +10,18 @@ FactoryBot.define do
         contributions_count { 3 }
       end
 
-      after(:build) do |success, evaluator|
-        create_list(:contribution, evaluator.contributions_count, success: success)
+      # after(:build) do |success, evaluator|
+      #   build_list(:contribution, evaluator.contributions_count, success: success)
+      # end
+
+      contributions do
+        Array.new(contributions_count) { association(:contribution) }
+      end
+    end
+
+    factory :success_with_story_categories do
+      after(:create) do |success, evaluator|
+        create_list(:story_category, 3, company: success.company, successes: [success])
       end
     end
 
