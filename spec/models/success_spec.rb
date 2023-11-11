035c624209f8e64ecb7ee7e391e32ec44e2a11ef
Ryan Wilson <rycwilson@gmail.com>
2023-11-11 13:42:17 -0800

    adds nested attribute matchers to Success spec

--- spec/models/success_spec.rb
+++ spec/models/success_spec.rb
@@ -7,9 +7,19 @@ RSpec.describe Success, type: :model do
     it { is_expected.to belong_to(:customer) }
     it { is_expected.to have_one(:company).through(:customer) }
     it { is_expected.to belong_to(:curator).class_name('User').with_foreign_key('curator_id') }
+    
     it { is_expected.to have_many(:contributions).inverse_of(:success).dependent(:destroy) }
-    it { is_expected.to have_one(:story).optional.dependent(:destroy) }
+    it { is_expected.to have_many(:contributors).through(:contributions).source(:contributor) }
+    it { is_expected.to have_one(:story).dependent(:destroy) }
     it { is_expected.to have_and_belong_to_many(:story_categories) }
+    it { is_expected.to have_and_belong_to_many(:products) }
+    
+    # TODO: test for distinctness separately
+    it { is_expected.to have_many(:invitation_templates).through(:contributions) }
+    
+    # TODO: test reject_if separately
+    it { is_expected.to accept_nested_attributes_for(:contributions).allow_destroy(false) }
+    it { is_expected.to accept_nested_attributes_for(:customer).allow_destroy(false) }
   
     context 'when a story category is removed' do
       subject(:success) { create(:success_with_story_categories) }
