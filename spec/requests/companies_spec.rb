47f128a094f63bd478c024ea6f1e364e3e410525
Ryan Wilson <rycwilson@gmail.com>
2023-11-19 08:22:56 -0800

    modifies companies request spec

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -4,7 +4,7 @@ RSpec.configure do |config|
   config.include Rails.application.routes.url_helpers, type: :request
 end
 
-RSpec.describe "Companies", type: :request do
+RSpec.describe 'Companies', type: :request do
   let(:company) do 
     begin
       create(:company)
@@ -13,34 +13,40 @@ RSpec.describe "Companies", type: :request do
     end
   end
   let(:curator) { create(:curator, company_id: company.id) }
-  
-  describe 'dashboard' do
-    shared_examples 'tab panel' do |path|
-      it 'loads successfully' do
-        get dashboard_url(path, subdomain: company.subdomain)
-        expect(response.content_type).to eq('text/html; charset=utf-8')
-        expect(response).to have_http_status(200)
-      end
-    end
 
-    context 'when curator is signed in' do
-      before { sign_in(curator) }
+  shared_examples 'company document' do |path|
+    before { get root_url(subdomain: company.subdomain) + path }
 
-      context('when /prospect') { it_behaves_like 'tab panel', 'prospect' }
-      context('when /curate') { it_behaves_like 'tab panel', 'curate' }
-      context('when /promote') { it_behaves_like 'tab panel', 'promote' }
-      context('when /measure') { it_behaves_like 'tab panel', 'measure' }
-    end
-
-    context 'when curator is not signed in' do
-      it 'redirects to the sign in page' do
-        get dashboard_url('curate', subdomain: company.subdomain)
-        expect(response).to redirect_to(new_user_session_url(subdomain: company.subdomain))
-      end
+    it 'loads successfully' do
+      # get root_url(subdomain: company.subdomain) + path
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq('text/html; charset=utf-8')
+      expect(response.body).to match(/<title>Customer Stories: Account (Dashboard|Settings)<\/title>/)
     end
   end
 
-  # describe 'Profile' do
-  #   subject { get company_settings_url(subdomain: company.subdomain) }
-  # end
+  shared_examples 'redirect to auth' do |path|
+    it 'redirects to the sign in page' do
+      get root_url(subdomain: company.subdomain) + path
+      expect(response).to redirect_to(new_user_session_url(subdomain: company.subdomain))
+    end
+  end
+
+  context 'when curator is signed in' do
+    before { sign_in(curator) }
+
+    describe('GET /prospect') { it_behaves_like 'company document', 'prospect' }
+    describe('GET /curate') { it_behaves_like 'company document', 'curate' }
+    describe('GET /promote') { it_behaves_like 'company document', 'promote' }
+    describe('GET /measure') { it_behaves_like 'company document', 'measure' }
+    describe('GET /settings') { it_behaves_like 'company document', 'settings' }
+  end
+
+  context 'when curator is not signed in' do
+    describe('GET /prospect') { it_behaves_like 'redirect to auth', 'prospect' }
+    describe('GET /curate') { it_behaves_like 'redirect to auth', 'curate' }
+    describe('GET /promote') { it_behaves_like 'redirect to auth', 'promote' }
+    describe('GET /measure') { it_behaves_like 'redirect to auth', 'measure' }
+    describe('GET /settings') { it_behaves_like 'redirect to auth', 'settings' }
+  end
 end
