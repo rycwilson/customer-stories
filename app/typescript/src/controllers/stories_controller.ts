209aeacc6538c279c309dea197fa8221158a41c0
Ryan Wilson <rycwilson@gmail.com>
2023-11-29 10:13:08 -0800

    adds turbo:frame-render callback to stories

--- app/typescript/src/controllers/stories_controller.ts
+++ app/typescript/src/controllers/stories_controller.ts
@@ -5,42 +5,48 @@ import imagesLoaded from 'imagesloaded';
 export default class extends Controller<HTMLDivElement> {
   static targets = [
     'searchAndFilters',   // one container for xs and sm, another for md and lg
-    'filterSelect', 
     'filterMatchTypeRadio',
-    'gallery', 
-    'card', 
+    'filterSelect', 
     'curatorSelect', 
     'statusSelect', 
     'customerSelect',
     'categorySelect',
-    'productSelect'
+    'productSelect',
+    'turboFrame',
+    'gallery', 
+    'card', 
   ];
   declare readonly searchAndFiltersTargets: HTMLDivElement[];
-  declare readonly filterSelectTargets: HTMLSelectElement[];
   declare readonly filterMatchTypeRadioTargets: HTMLInputElement[];
-  declare readonly galleryTarget: HTMLDivElement;
-  declare readonly cardTargets: HTMLDivElement[];
+  declare readonly filterSelectTargets: HTMLSelectElement[];
   declare readonly curatorSelectTarget: HTMLSelectElement;
   declare readonly statusSelectTarget: HTMLSelectElement;
   declare readonly customerSelectTarget: HTMLSelectElement;
   declare readonly categorySelectTarget: HTMLSelectElement;
   declare readonly productSelectTarget: HTMLSelectElement;
+  declare readonly turboFrameTarget: FrameElement;
+  declare readonly galleryTarget: HTMLDivElement;
+  declare readonly cardTargets: HTMLDivElement[];
 
   static values = { filterMatchType: String };
   declare filterMatchTypeValue: string;
   
   readyFilters = 0;
 
+
   connect() {
     // console.log('connect stories')
-    imagesLoaded('#stories-gallery', (instance) => {
-      // console.log('images loaded', e)
-      this.galleryTarget.classList.remove('hidden');
-    })
-
     this.filterMatchTypeRadioTargets.forEach(input => {
       input.checked = input.value === this.filterMatchTypeValue;
     })
+
+    // document.documentElement.addEventListener('turbo:frame-render', this.onGalleryRender.bind(this));
+    this.turboFrameTarget.addEventListener('turbo:frame-render', this.onGalleryRender.bind(this));
+  }
+
+  disconnect() {
+    // no need for this since listener is attached to the frame (which will disappear along with its listeners when this disconnects)
+    // document.documentElement.removeEventListener('turbo:frame-render', this.onGalleryRender.bind(this));
   }
 
   onInitFilter(e: Event) {
@@ -70,5 +76,15 @@ export default class extends Controller<HTMLDivElement> {
       newSrc.searchParams.set(type, id);
       turboFrame.src = newSrc.toString();
     }
+  }
+
+  onGalleryRender(e: Event) {
+    console.log(e)
+    const frame = e.target as FrameElement;
+    // if (frame.id !== 'stories') return;
+    imagesLoaded('#stories-gallery', (instance) => {
+      // console.log('images loaded', instance)
+      this.galleryTarget.classList.remove('hidden');
+    })
   }
 }
