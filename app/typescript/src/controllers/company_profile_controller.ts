daf73881f745eac25db373c1569c13f7d1726f80
Ryan Wilson <rycwilson@gmail.com>
2023-12-24 10:53:56 -0800

    adds hex to rgb utility function

--- app/typescript/src/controllers/company_profile_controller.ts
+++ app/typescript/src/controllers/company_profile_controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
+import { hexToRgb } from '../utils';
 
 export default class CompanyProfileController extends Controller<HTMLFormElement> {
   connect() {
