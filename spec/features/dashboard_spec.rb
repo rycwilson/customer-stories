dc68f3f7c4e884470bb6c79e70402525c62b3d01
Ryan Wilson <rycwilson@gmail.com>
2024-01-04 09:06:46 -0800

    refactors

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -6,18 +6,20 @@ end
 
 # RSpec.describe 'Dashboard', type: :feature do
 RSpec.feature 'Dashboard' do
-  let(:company) { create(:company) }
-  let(:curator) { create(:curator, company_id: company.id) }
+  # let(:company) { create(:company) }
+  # let(:curator) { create(:curator, company_id: company.id) }
+  # before { sign_in(curator) }
 
-  describe 'tab interface' do
-    before { sign_in(curator) }
+  given(:company) { create(:company) }
+  given(:curator) { create(:curator, company_id: company.id) }
+  background { sign_in(curator) }
 
-    scenario 'active tab follows the URL path' do
-      %w(prospect curate promote measure).each do |path|
-        visit root_url(subdomain: company.subdomain) + path
-        expect(page).to have_css("li.active > a[href='##{path}']")
-        expect(page).to have_css("##{path}.tab-pane.active")
-      end
+  # it 'active tab follows the URL path' do
+  scenario 'active tab follows the URL path' do
+    %w(prospect curate promote measure).each do |path|
+      visit root_url(subdomain: company.subdomain) + path
+      expect(page).to have_css("li.active > a[href='##{path}']")
+      expect(page).to have_css("##{path}.tab-pane.active")
     end
   end
 end
