b812b19e3aa8f312089049a57201a580c07b36a4
Ryan Wilson <rycwilson@gmail.com>
2024-01-18 20:35:39 -0800

    adds attribute to customer factory so that allows for featured stories query

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :customer, class: 'Customer' do
     name { Faker::Company.name }
+    logo_url { 'must have a value or stories can not be featured' }
     skip_callbacks { true }
     company
   end
