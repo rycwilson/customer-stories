497662006301531aae5c9f8a05ed81a456606d53
Ryan Wilson <rycwilson@gmail.com>
2024-02-21 12:47:35 -0800

    removes babel packages

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-// File created per https://jestjs.io/docs/getting-started#using-typescript
-// (converted from CommonJS to ES module via typescipt tooling)
-
-export const presets = [
-  ['@babel/preset-env', { targets: { node: 'current' } }],
-  '@babel/preset-typescript'
-];
