b10e6d79e83acb7943f25b77a9b5eceba4fa77d0
Ryan Wilson <rycwilson@gmail.com>
2024-02-21 09:58:09 -0800

    adds babel config file

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+// File created per https://jestjs.io/docs/getting-started#using-typescript
+// (converted from CommonJS to ES module via typescipt tooling)
+
+export const presets = [
+  ['@babel/preset-env', { targets: { node: 'current' } }],
+  '@babel/preset-typescript'
+];
