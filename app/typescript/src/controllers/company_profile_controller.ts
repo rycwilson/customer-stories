3ca44606f34fb1ded355a0b6f037d56cb2f18d1d
Ryan Wilson <rycwilson@gmail.com>
2024-04-13 19:21:33 -0700

    extends form controller for company profile

--- app/typescript/src/controllers/company_profile_controller.ts
+++ app/typescript/src/controllers/company_profile_controller.ts
@@ -1,7 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
+import FormController from './form_controller';
 import { hexToRgb } from '../utils';
 
-export default class CompanyProfileController extends Controller<HTMLFormElement> {
+export default class CompanyProfileController extends FormController<CompanyProfileController> {
   connect() {
     console.log('connect company profile')
   }
