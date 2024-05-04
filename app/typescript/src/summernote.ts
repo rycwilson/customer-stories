c1e622d237ef735655a4583531ed6c5b07dc802c
Ryan Wilson <rycwilson@gmail.com>
2024-05-04 14:57:23 -0700

    adds summernote module for custom type definitions

---
+++ app/typescript/src/summernote.ts
@@ -0,0 +1,28 @@
+// list of all possible custom buttons
+type CustomButton = 'placeholdersDropdown' | 'contributionsDropdown';
+
+// copied from @types/summernote
+type ToolbarDefinition = Array<
+  | ["style", Summernote.toolbarStyleGroupOptions[]]
+  | ["font", Summernote.toolbarFontGroupOptions[]]
+  | ["fontname", Summernote.toolbarFontNameOptions[]]
+  | ["fontsize", Summernote.toolbarFontsizeGroupOptions[]]
+  | ["color", Summernote.toolbarColorGroupOptions[]]
+  | ["para", Summernote.toolbarParaGroupOptions[]]
+  | ["height", Summernote.toolbarHeightGroupOptions[]]
+  | ["table", Summernote.toolbarTableGroupOptions[]]
+  | ["insert", Summernote.toolbarInsertGroupOptions[]]
+  | ["view", Summernote.toolbarViewGroupOptions[]]
+  | ["help", Summernote.toolbarHelpGroupOptions[]]
+  | ["misc", Summernote.miscGroupOptions[]]
+  | ["customButton", CustomButton[]]
+>;
+
+type CustomOptions = {
+  toolbar: ToolbarDefinition;
+  buttons: Partial<{ [key in CustomButton]: (context: any) => JQuery<HTMLDivElement, any> }>
+}
+
+export type SummernoteEditorKind = 'winStory' | 'story' | 'invitationTemplate' | 'default';
+
+export type CustomSummernoteOptions = Summernote.Options & CustomOptions;
