1f29bb2d4960ac406d8b4a9e80ed049db5b045ed
Ryan Wilson <rycwilson@gmail.com>
2024-06-26 15:04:27 -0700

    adds status enum to ContributorInvitation

--- app/models/contributor_invitation.rb
+++ app/models/contributor_invitation.rb
@@ -1,3 +1,5 @@
 class ContributorInvitation < ApplicationRecord
-  belongs_to :contribution_id
+  belongs_to :contribution
+
+  enum status: { pending: 0, sent: 1 }
 end
--- db/migrate/20240625222050_create_contributor_invitations.rb
+++ db/migrate/20240625222050_create_contributor_invitations.rb
@@ -3,8 +3,9 @@ class CreateContributorInvitations < ActiveRecord::Migration[6.1]
     create_table :contributor_invitations do |t|
       t.text :email_subject
       t.text :email_body
+      t.integer :status, default: 0
       t.datetime :sent_at
-      t.references :contribution_id, foreign_key: true
+      t.references :contribution, index: true, foreign_key: true
 
       t.timestamps
     end
