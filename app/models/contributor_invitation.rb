695a01e3065538834edac79c20ab965d6284547f
Ryan Wilson <rycwilson@gmail.com>
2024-06-28 12:56:34 -0700

    fixes ContributorInvitation#populate_template

--- app/models/contributor_invitation.rb
+++ app/models/contributor_invitation.rb
@@ -6,12 +6,12 @@ class ContributorInvitation < ApplicationRecord
   enum status: { pending: 0, sent: 1 }
 
   def populate_template
-    email_subject = contribution.invitation_template.request_subject
+    self.email_subject = contribution.invitation_template.request_subject
       .gsub('[customer_name]', contribution.customer.name)
       .gsub('[company_name]', contribution.company.name)
       .gsub('[contributor_first_name]', contribution.contributor.first_name)
       .gsub('[contributor_full_name]', contribution.contributor.full_name)
-    email_body = contribution.invitation_template.request_body
+    self.email_body = contribution.invitation_template.request_body
       .gsub('[customer_name]', contribution.customer.name)
       .gsub('[company_name]', contribution.company.name)
       .gsub('[contributor_first_name]', contribution.contributor.first_name)
@@ -23,6 +23,6 @@ class ContributorInvitation < ApplicationRecord
       .gsub('[curator_img_url]', contribution.curator.photo_url || '')
       .gsub('[contribution_submission_url]', contribution.invitation_link('contribution'))
       .gsub('[feedback_submission_url]', contribution.invitation_link('feedback'))
-      .html_safe
+    return self
   end
 end
