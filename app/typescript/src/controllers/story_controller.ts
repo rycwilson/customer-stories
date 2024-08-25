a44b8212d697951cdb46e066056d29804f81cd4a
Ryan Wilson <rycwilson@gmail.com>
2024-08-24 17:37:42 -0700

    adds resize handler to story controller

--- app/typescript/src/controllers/story_controller.ts
+++ app/typescript/src/controllers/story_controller.ts
@@ -1,22 +1,26 @@
 import { Controller } from '@hotwired/stimulus';
+import { debounce } from '../utils';
 
 export default class StoryController extends Controller<HTMLDivElement> {
   static targets = [
-    'storySettingsForm', 
-    'storyContentForm', 
+    'titleInput',
+    'narrativeTextarea',
     'hiddenLinkInput', 
     'hiddenLinkCopyBtn',
     'contributors',
   ];
-  declare readonly storySettingsFormTarget: HTMLFormElement;
-  declare readonly storyContentFormTarget: HTMLFormElement;
+  declare readonly titleInputTargets: HTMLInputElement[];
+  declare readonly narrativeTextareaTargets: HTMLTextAreaElement[];
   declare readonly hiddenLinkInputTarget: HTMLInputElement;
   declare readonly hiddenLinkCopyBtnTarget: HTMLButtonElement;
   declare readonly contributorsTarget: HTMLDivElement;
+
+  resizeHandler = debounce(this.onResize.bind(this), 200);
+  currentScreen: ScreenSize | undefined = undefined;
  
   connect() {
-    // this.storySettingsFormTarget.setAttribute('data-init', 'true');
     this.contributorsTarget.setAttribute('data-resource-init-value', 'true');
+    window.addEventListener('resize', this.resizeHandler);
   }
 
   refreshHiddenLink() {
@@ -25,9 +29,39 @@ export default class StoryController extends Controller<HTMLDivElement> {
     const hiddenLink = window.location.origin + '/' + Date.now().toString(36) + Math.random().toString(36).substring(2);
     this.hiddenLinkInputTarget.value = hiddenLink;
     this.hiddenLinkCopyBtnTarget.setAttribute('title', 'Save changes to enable Copy');
+    window.removeEventListener('resize', this.resizeHandler);
 
     // $('.hidden-link__copy')
     //   .tooltip('fixTitle')
     //   .addClass('disabled')
+  }
+
+  onInitNarrativeEditor({ target: textarea }: { target: HTMLTextAreaElement }) {
+    if (textarea.checkVisibility()) {
+      this.currentScreen = <ScreenSize>textarea.id.match(/(?<screen>(sm|md-lg)$)/)?.groups?.screen;
+    }
+  }
+
+  get visibleNarrativeTextarea() {
+    return this.narrativeTextareaTargets.find(textarea => {
+      const editor = <HTMLElement>textarea.nextElementSibling;
+      return editor.checkVisibility();
+    });
+  }
+
+  onResize() {
+    if (this.visibleNarrativeTextarea) {
+      const newTextarea = this.visibleNarrativeTextarea;
+      const newScreen = <ScreenSize>newTextarea.id.match(/(?<screen>(sm|md-lg)$)/)?.groups?.screen
+      const shouldSyncView = newScreen !== this.currentScreen;
+      if (shouldSyncView) {
+        const oldTextarea = this.narrativeTextareaTargets.find(textarea => textarea !== newTextarea);
+        const newTitle = <HTMLInputElement>this.titleInputTargets.find(input => input.id.includes(newScreen));
+        const oldTitle = <HTMLInputElement>this.titleInputTargets.find(input => input !== newTitle);
+        $(newTextarea).summernote('code', $(oldTextarea).summernote('code'));
+        newTitle.value = oldTitle.value;
+        this.currentScreen = newScreen;
+      }
+    }
   }
 }
