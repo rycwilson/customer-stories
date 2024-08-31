d95b31c0e43d36d1d93f389d7562ae405d794f6a
Ryan Wilson <rycwilson@gmail.com>
2024-08-31 13:10:03 -0700

    modifies CallToAction to accept nested company params

--- app/models/call_to_action.rb
+++ app/models/call_to_action.rb
@@ -2,4 +2,5 @@ class CallToAction < ApplicationRecord
   belongs_to :company
   has_and_belongs_to_many :successes, join_table: 'ctas_successes'
   has_many :stories, through: :successes
+  accepts_nested_attributes_for :company, reject_if: -> (attrs) { !attrs.keys.match(/primary_cta/) }
 end
