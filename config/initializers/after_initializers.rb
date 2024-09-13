789981468eb447f893b75a803af523db5f61272a
Ryan Wilson <rycwilson@gmail.com>
2024-09-12 17:57:47 -0700

    removes turbo and stimulus js assets from precompile list

--- config/initializers/after_initializers.rb
+++ config/initializers/after_initializers.rb
@@ -1,3 +1,12 @@
 Rails.application.config.after_initialize do
-  Rails.application.config.assets.precompile -= %w( doorkeeper/application.css doorkeeper/admin/application.css )
+  Rails.application.config.assets.precompile -= %w( 
+    doorkeeper/application.css 
+    doorkeeper/admin/application.css 
+    turbo.js
+    turbo.min.js
+    turbo.min.js.map
+    stimulus.js
+    stimulus.min.js
+    stimulus.min.js.map
+  )
 end
