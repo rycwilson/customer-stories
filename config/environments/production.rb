326137418e48c53e8e4d15410df1e7fb8a29a281
Ryan Wilson <rycwilson@gmail.com>
2024-09-19 10:30:10 -0700

    moves config.assets.prefix setting from dev to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.prefix = "/dev-assets"
   config.assets.gzip = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
