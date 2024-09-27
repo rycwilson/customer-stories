a767b24541d1de6c45226e4c830329b2d16b0547
Ryan Wilson <rycwilson@gmail.com>
2024-09-26 20:36:49 -0700

    removes obsoleted

--- app/typescript/src/controllers/ads_controller.ts
+++ app/typescript/src/controllers/ads_controller.ts
@@ -13,33 +13,16 @@ export default class AdsController extends FormController<AdsController> {
   declare readonly newImageCardTarget: HTMLLIElement;
   declare readonly newLogoCardTarget: HTMLLIElement;
 
-  // declare inputObserver: MutationObserver;
-  // declare imageTimer: number;
-
-  // changeFileInputHandler = this.onChangeFileInput.bind(this);
-  // validatedFileInputHandler = this.onValidatedFileInput.bind(this);
-  // validFileInputHandler = this.onValidFileInput.bind(this);
-  // invalidFileInputHandler = this.onInvalidFileInput.bind(this);
-  
   connect() {
     super.connect();
     // jquery event listeners necessary for hooking into jquery plugin events
     $(this.element).validator(imageValidatorOptions);
-      // .on('change.bs.fileinput', '.ad-image-card', this.changeFileInputHandler)
-      // .on('validated.bs.validator', this.validatedFileInputHandler)
-      // .on('valid.bs.validator', this.validFileInputHandler)
-      // .on('invalid.bs.validator', this.invalidFileInputHandler)
-
     this.imageRequirementsTargets.forEach(this.initPopover);
   }
 
   disconnect() {
     super.disconnect();
     $(this.element).validator('destroy');
-      // .off('change.bs.fileinput', '.ad-image-card', this.changeFileInputHandler)
-      // .off('validated.bs.validator', this.validatedFileInputHandler)
-      // .off('valid.bs.validator', this.validFileInputHandler)
-      // .off('invalid.bs.validator', this.invalidFileInputHandler)
   }
 
   submitForm({ 
@@ -86,63 +69,6 @@ export default class AdsController extends FormController<AdsController> {
     $(this.element).validator('update').validator('validate');
   }
 
-  // onChangeFileInput({ target: formGroup, currentTarget: card }: { target: HTMLDivElement, currentTarget: HTMLLIElement }) {
-  //   if (formGroup.classList.contains('fileinput')) this.handleS3Upload(formGroup, card);
-  // }
-
-  // handleS3Upload(formGroup: HTMLDivElement, card: HTMLLIElement) {
-  //   const img = <HTMLImageElement>card.querySelector('img');
-  //   const urlInput = <HTMLInputElement>card.querySelector(':scope > input[name*="[image_url]"]');
-  //   const isSuccessfulUpload = (mutation: MutationRecord): boolean => {
-  //     const { target, type, attributeName } = mutation;
-  //     return formGroup.classList.contains('has-error') ?
-  //       false :
-  //       target === urlInput && type === 'attributes' && attributeName === 'value';
-  //   };
-  //   this.inputObserver = new MutationObserver(mutations => {
-  //     for (const m of mutations) {
-  //       if (isSuccessfulUpload(m)) {
-  //         this.inputObserver.disconnect();
-  //         console.log('isSuccessfulUpload', urlInput.value)
-
-  //         if (card.classList.contains('gads-default.has-image')) {
-  //           const idInput = <HTMLInputElement>card.querySelector(':scope > input[name*="[id]"]');
-  //           const prevDefaultId = idInput.value;
-  //           this.keepPreviousDefault(prevDefaultId);
-  //           idInput.value = '';
-  //         }
-
-  //         // pre-load the image so it will be in browser cache when response arrives (no flicker)
-  //         img.addEventListener(
-  //           'load', 
-  //           () => (<HTMLButtonElement>formGroup.querySelector('.btn-success')).dispatchEvent(new Event('click')),
-  //           { once: true }
-  //         )
-  //         img.setAttribute('src', urlInput.value);
-  //         break;
-  //       }
-  //     };
-  //   });
-  //   this.inputObserver.observe(urlInput, { attributes: true });
-  //   if (!this.imageDidLoad(card, img)) {
-  //     this.imageTimer = window.setInterval(this.imageDidLoad.bind(this, card, img), 100);
-  //   }
-  // }
-
-  // imageDidLoad(card: HTMLLIElement, img: HTMLImageElement) {
-  //   if (img.complete) {
-  //     window.clearInterval(this.imageTimer);
-  //     console.log('image did load')
-
-  //     // the data-validate attribute is to prevent premature validation (per bootstrap-validator)
-  //     if (card.classList.contains('gads-default')) card.classList.add('ad-image-card--new');
-  //     const fileInput = <HTMLInputElement>card.querySelector('input[type="file"]')
-  //     fileInput.setAttribute('data-validate', 'true');
-  //     $(this.element).validator('update').validator('validate');
-  //     return true;
-  //   }
-  // }
-
   keepPreviousDefault(id: string) {
     const i = this.imageCardTargets.length;
     this.element.insertAdjacentHTML('beforeend', `
@@ -151,33 +77,6 @@ export default class AdsController extends FormController<AdsController> {
       <input class="hidden" type="checkbox" name="company[adwords_images_attributes][${i}][default]" value="true">
     `);
   }
-
-  // onValidatedFileInput({ relatedTarget: input }: { relatedTarget: HTMLInputElement }) {
-  //   if (input.type === 'file' && !input.getAttribute('data-default-type')) {
-  //     console.log('validated.bs.validator', input)
-  //     const card = <HTMLLIElement>input.closest('.ad-image-card--new');
-  //     card.classList.remove('hidden');
-  //   }
-  // }
-
-  // onValidFileInput({ relatedTarget: input }: { relatedTarget: HTMLInputElement }) {
-  //   const isNewImage = input.type === 'file' && input.value;
-  //   if (isNewImage) {
-  //     console.log('valid.bs.validator', input)
-  //     // initS3Upload($(e.currentTarget), $input);
-  //     // initS3FileInput(input);
-  //     $(input).fileupload('send', { files: input.files });
-
-  //     // Change event on the input will trigger the s3 upload
-  //     // => stop the event propagation so that the upload handler does not re-execute
-  //     // $input.closest('.ad-image-card').one('change.bs.fileinput', () => false);
-  //     // $input.trigger('change');
-  //   }
-  // }
-
-  // // onInvalidFileInput() {
-  // //   this.inputObserver.disconnect();
-  // // }
 
   setNewDefault(
     { detail: { card, kind, toggleDefault } }: { detail: { card: HTMLLIElement, kind: AdImageKind, toggleDefault: boolean } }
