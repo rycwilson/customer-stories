dec6c931247bd5c13b29b5367c49769eae9cbc63
Ryan Wilson <rycwilson@gmail.com>
2024-10-01 13:17:01 -0700

    enables s3 auto delete

--- lib/s3_util.rb
+++ lib/s3_util.rb
@@ -3,14 +3,14 @@ module S3Util
     # delete from csp-dev-assets bucket OR from .net production environment
     # do not delete from csp-prod-assets bucket except from .net production environment
     def delete_object(bucket, object_url)
-      # key = object_url.match(/(amazonaws\.com|cloudfront\.net)\/(.*)/).try(:[], 2)
-      # if key && (object_url.include?('csp-dev-assets') || ENV['HOST_NAME'].include?('.net'))
-      #   bucket.delete_objects({
-      #     delete: { 
-      #       objects: [{ key: key }] 
-      #     }
-      #   })
-      # end
+      key = object_url.match(/(amazonaws\.com|cloudfront\.net)\/(.*)/).try(:[], 2)
+      if key && (object_url.include?('csp-dev-assets') || ENV['HOST_NAME'].include?('.net'))
+        bucket.delete_objects({
+          delete: { 
+            objects: [{ key: }] 
+          }
+        })
+      end
     end
   end
 end
