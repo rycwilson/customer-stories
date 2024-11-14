c72806cef15bd85e4552e3ad2f86303237549596
Ryan Wilson <rycwilson@gmail.com>
2024-11-13 19:21:31 -0800

    adds image card to customer form

--- app/typescript/src/controllers/customer_controller.ts
+++ app/typescript/src/controllers/customer_controller.ts
@@ -1,8 +1,19 @@
 import FormController from './form_controller';
+import { imageValidatorOptions } from '../user_uploads';
 
 export default class CustomerController extends FormController<CustomerController> {
   static targets = ['logoContainer'];
   declare readonly logoContainerTarget: HTMLDivElement;
+
+  connect() {
+    super.connect();
+    $(this.element).validator(imageValidatorOptions);
+  }
+
+  disconnect() {
+    super.disconnect()
+    $(this.element).validator('destroy');
+  }
 
   toggleShowName(e: Event) {
     this.logoContainerTarget.classList.toggle('with-customer-name');
