aed90ff60b6f74755846f2d43e0a21ed4ba88eb0
Ryan Wilson <rycwilson@gmail.com>
2024-11-14 15:30:59 -0800

    adds customer name togglnig to customer form image card

--- app/typescript/src/controllers/customer_controller.ts
+++ app/typescript/src/controllers/customer_controller.ts
@@ -2,20 +2,13 @@ import FormController from './form_controller';
 import { imageValidatorOptions } from '../user_uploads';
 
 export default class CustomerController extends FormController<CustomerController> {
-  static targets = ['logoContainer'];
-  declare readonly logoContainerTarget: HTMLDivElement;
-
   connect() {
     super.connect();
     $(this.element).validator(imageValidatorOptions);
   }
 
   disconnect() {
-    super.disconnect()
     $(this.element).validator('destroy');
-  }
-
-  toggleShowName(e: Event) {
-    this.logoContainerTarget.classList.toggle('with-customer-name');
+    super.disconnect();
   }
 }
