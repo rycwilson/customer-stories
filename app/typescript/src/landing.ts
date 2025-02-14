d16728695afee58c8450f792c3890eba241560c2
Ryan Wilson <rycwilson@gmail.com>
2025-02-14 15:20:20 -0800

    adds bootoast to devise views

--- app/typescript/src/landing.ts
+++ app/typescript/src/landing.ts
@@ -1,15 +1,42 @@
 import './jquery';
 import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/transition';
 import 'bootstrap-sass-3.3.6/assets/javascripts/bootstrap/collapse';
+import bootoast from 'bootoast';
 import { toggleHeaderOnScroll, validateForm } from './utils';
 
 const header = <HTMLElement>document.body.querySelector(':scope > nav');
-const signInForm: HTMLFormElement | null = document.querySelector('form[action="/users/sign_in"]'); 
-const signUpForm: HTMLFormElement | null = document.querySelector('form[action="/users"]');
+const accountForm: HTMLFormElement | null = document.querySelector('.account-form');
+const toasterBaseOptions = {
+  timeout: 4,
+  animationDuration: 150,
+  dismissable: true
+}
 
 window.addEventListener('scroll', toggleHeaderOnScroll(header), { passive: true });
-if (signInForm) signInForm.addEventListener('submit', validateForm);
-if (signUpForm) signUpForm.addEventListener('submit', validateForm);
+
+if (accountForm) {
+  accountForm.addEventListener('submit', validateForm);
+  if (accountForm.dataset.flash) {
+    const flash = JSON.parse(accountForm.dataset.flash);
+    Object.keys(flash).forEach((flashType: string) => {
+      const type = (() => {
+        if (flashType === 'notice') {
+          return 'success';
+        } else if (flashType === 'alert') {
+          return 'danger';
+        } else {
+          return flashType;
+        }
+      })();
+      const message = flash[flashType];
+      bootoast.toast({ ...toasterBaseOptions, type, message, position: 'top-center' });
+    });
+  } else if (accountForm.dataset.resourceErrors) {
+    JSON.parse(accountForm.dataset.resourceErrors).forEach((error: string) => {
+      bootoast.toast({ ...toasterBaseOptions, type: 'danger', message: error, position: 'top-center' }); 
+    });
+  }
+}
 
 let resizeTimer: number;
 setViewportHeight();
