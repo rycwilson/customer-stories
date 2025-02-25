5f38ea95cf37d2804dfc124b33eba307c65e8454
Ryan Wilson <rycwilson@gmail.com>
2025-02-25 14:47:41 -0800

    enables devise confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,12 +59,10 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :lockable#, :confirmable
-  devise :doorkeeper
-
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :lockable, :confirmable
+  
   # doorkeeper
+  devise :doorkeeper
   has_many :access_grants, class_name: "Doorkeeper::AccessGrant", foreign_key: :resource_owner_id, dependent: :delete_all # or :destroy if you need callbacks
   has_many :access_tokens, class_name: "Doorkeeper::AccessToken", foreign_key: :resource_owner_id, dependent: :delete_all # or :destroy if you need callbacks
 
