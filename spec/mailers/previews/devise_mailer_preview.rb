07b941610e481713e13cf80bc4035fec000cadeb
Ryan Wilson <rycwilson@gmail.com>
2025-03-01 15:01:00 -0800

    adds email parameter to confirmation instructions preview

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -1,7 +1,7 @@
 class DeviseMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/devise_mailer/confirmation_instructions
   def confirmation_instructions
-    user = User.find_by_email 'ryan@ryanwilson.dev'
+    user = User.find_by_email params['email']
     Devise::Mailer.confirmation_instructions(user, user.confirmation_token)
   end
 end
