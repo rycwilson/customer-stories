0479c7f681b36702f8a71a5c0fd263fe80de4277
Ryan Wilson <rycwilson@gmail.com>
2025-03-03 09:03:21 -0800

    modifies devise passwords controller

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,30 +1,36 @@
 class Users::PasswordsController < Devise::PasswordsController
-  layout('landing', only: [:new])
+  layout('landing')
+  
   # GET /resource/password/new
-   def new
-     super
-   end
+  def new
+    if request.subdomain.present? or request.path == new_user_password_path
+      redirect_to(new_csp_password_url(subdomain: nil), status: :moved_permanently)
+      return
+    end
+    super
+  end
 
   # POST /resource/password
-   def create
-     super
-   end
+  def create
+    super
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-   def edit
-     super
-   end
+  def edit
+    super
+  end
 
   # PUT /resource/password
-   def update
-     super
-   end
+  def update
+    super
+  end
 
   # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    # super(resource)
+    edit_user_url(subdomain: resource.company&.subdomain)
+  end
 
   # The path used after sending reset password instructions
   # def after_sending_reset_password_instructions_path_for(resource_name)
