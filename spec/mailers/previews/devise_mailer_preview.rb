3ced922b087e71ef16cea2dfa80c4930bbebd853
Ryan Wilson <rycwilson@gmail.com>
2025-03-03 08:58:54 -0800

    adds devise mailer preview for reset password action

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -4,4 +4,9 @@ class DeviseMailerPreview < ActionMailer::Preview
     user = User.find_by_email params['email']
     Devise::Mailer.confirmation_instructions(user, user.confirmation_token)
   end
+
+  def reset_password_instructions
+    user = User.find_by_email params['email']
+    Devise::Mailer.reset_password_instructions(user, user.reset_password_token)
+  end
 end
