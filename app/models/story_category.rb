d237263de43dfa9c83b05b68f0b3ec465f2b8444
Ryan Wilson <rycwilson@gmail.com>
2025-04-29 15:28:25 -0700

    removes obsoleted

--- app/models/story_category.rb
+++ app/models/story_category.rb
@@ -11,11 +11,6 @@ class StoryCategory < ApplicationRecord
 
   friendly_id :name, use: [:slugged, :scoped], scope: :company_id
 
-  after_commit do 
-    self.company.expire_fragment_cache('plugin-config')
-    self.company.expire_fragment_cache('story-tags')
-  end
-
   scope :featured, -> { joins(:stories).merge(Story.featured).distinct }
 
   def should_generate_new_friendly_id?
