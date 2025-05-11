76b9c6fa49c4aa6e5b0e0c2c393452ca501bc0af
Ryan Wilson <rycwilson@gmail.com>
2025-05-11 11:58:52 -0700

    removes obsoleted

--- app/typescript/src/controllers/story_controller.ts
+++ app/typescript/src/controllers/story_controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
-import { debounce } from '../utils';
+// import { debounce } from '../utils';
 
 export default class StoryController extends Controller<HTMLDivElement> {
   static targets = [
@@ -17,12 +17,12 @@ export default class StoryController extends Controller<HTMLDivElement> {
   declare readonly contributionsTarget: HTMLDivElement;
   declare readonly resultsListTarget: HTMLUListElement;
 
-  declare currentScreen: ScreenSize;
-  resizeHandler = debounce(this.onResize.bind(this), 200);
+  // declare currentScreen: ScreenSize;
+  // resizeHandler = debounce(this.onResize.bind(this), 200);
  
   connect() {
     this.contributionsTarget.setAttribute('data-resource-init-value', 'true');
-    window.addEventListener('resize', this.resizeHandler);
+    // window.addEventListener('resize', this.resizeHandler);
   }
 
   refreshHiddenLink() {
@@ -31,38 +31,38 @@ export default class StoryController extends Controller<HTMLDivElement> {
     const hiddenLink = window.location.origin + '/' + Date.now().toString(36) + Math.random().toString(36).substring(2);
     this.hiddenLinkInputTarget.value = hiddenLink;
     this.hiddenLinkCopyBtnTarget.setAttribute('title', 'Save changes to enable Copy');
-    window.removeEventListener('resize', this.resizeHandler);
+    // window.removeEventListener('resize', this.resizeHandler);
 
     // $('.hidden-link__copy')
     //   .tooltip('fixTitle')
     //   .addClass('disabled')
   }
 
-  onInitNarrativeEditor({ target: textarea }: { target: HTMLTextAreaElement }) {
-    if (textarea.checkVisibility()) {
-      this.currentScreen = <ScreenSize>textarea.id.match(/(?<screen>(sm|md-lg)$)/)?.groups?.screen;
-    }
-  }
+  // onInitNarrativeEditor({ target: textarea }: { target: HTMLTextAreaElement }) {
+  //   if (textarea.checkVisibility()) {
+  //     this.currentScreen = <ScreenSize>textarea.id.match(/(?<screen>(sm|md-lg)$)/)?.groups?.screen;
+  //   }
+  // }
 
-  get visibleNarrativeTextarea() {
-    return this.narrativeTextareaTargets.find(textarea => {
-      const editor = <HTMLElement>textarea.nextElementSibling;
-      return editor.checkVisibility();
-    });
-  }
+  // get visibleNarrativeTextarea() {
+  //   return this.narrativeTextareaTargets.find(textarea => {
+  //     const editor = <HTMLElement>textarea.nextElementSibling;
+  //     return editor.checkVisibility();
+  //   });
+  // }
 
-  onResize() {
-    const newNote = this.visibleNarrativeTextarea;
-    const newScreen = <ScreenSize>newNote?.id.match(/(?<screen>(sm|md-lg)$)/)?.groups?.screen || 'xs';
-    if (newScreen === this.currentScreen || newScreen === 'xs' || this.currentScreen === 'xs') {
-      this.currentScreen = newScreen;
-      return;
-    }
-    const oldNote = this.narrativeTextareaTargets.find(textarea => textarea !== newNote);
-    const newTitle = <HTMLInputElement>this.titleInputTargets.find(input => input.id.includes(newScreen));
-    const oldTitle = <HTMLInputElement>this.titleInputTargets.find(input => input !== newTitle);
-    $(newNote).summernote('code', $(oldNote).summernote('code'));
-    newTitle.value = oldTitle.value;
-    this.currentScreen = newScreen;
-  }
+  // onResize() {
+  //   const newNote = this.visibleNarrativeTextarea;
+  //   const newScreen = <ScreenSize>newNote?.id.match(/(?<screen>(sm|md-lg)$)/)?.groups?.screen || 'xs';
+  //   if (newScreen === this.currentScreen || newScreen === 'xs' || this.currentScreen === 'xs') {
+  //     this.currentScreen = newScreen;
+  //     return;
+  //   }
+  //   const oldNote = this.narrativeTextareaTargets.find(textarea => textarea !== newNote);
+  //   const newTitle = <HTMLInputElement>this.titleInputTargets.find(input => input.id.includes(newScreen));
+  //   const oldTitle = <HTMLInputElement>this.titleInputTargets.find(input => input !== newTitle);
+  //   $(newNote).summernote('code', $(oldNote).summernote('code'));
+  //   newTitle.value = oldTitle.value;
+  //   this.currentScreen = newScreen;
+  // }
 }
