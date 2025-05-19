61c1f54af2f3b39294ea9440af3f473354b2ec89
Ryan Wilson <rycwilson@gmail.com>
2025-05-19 12:23:57 -0700

    adds turbo:submit-end handler to new story controller (currently unused)

--- app/typescript/src/controllers/new_story_controller.ts
+++ app/typescript/src/controllers/new_story_controller.ts
@@ -1,8 +1,10 @@
 import FormController from "./form_controller";
+import type { TurboSubmitEndEvent, TurboVisitEvent, FetchResponse } from "@hotwired/turbo";
 
 export default class NewStoryController extends FormController<NewStoryController> {
   static targets = [...FormController.targets, 'storyTitle', 'successPlaceholder']
   declare readonly storyTitleTarget: HTMLInputElement;
+  declare readonly curatorSelectTarget: TomSelectInput;
 
   handleShownModal = this.onShownModal.bind(this);
 
@@ -18,5 +20,22 @@ export default class NewStoryController extends FormController<NewStoryControlle
 
   onShownModal() {
     this.storyTitleTarget.focus();
+  }
+
+  onTurboSubmitEnd({ detail: { fetchResponse: { response } } } : { detail: { fetchResponse: FetchResponse } }) {
+    const redirectUrl = response.headers.get('Location');
+    if (response.ok && redirectUrl) {
+      document.documentElement.addEventListener(
+        'turbo:load', 
+        (e: TurboVisitEvent) => {
+          const toaster = document.getElementById('toaster');
+          if (toaster) {
+            toaster.setAttribute('data-toast-flash-value', JSON.stringify({ notice: 'Story created successfully' }));
+          }
+        },
+        { once: true}
+      )
+      Turbo.visit(redirectUrl);  
+    }
   }
 }
