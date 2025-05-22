ad300e437b8ecac4a3c328d171404b9bae01c23a
Ryan Wilson <rycwilson@gmail.com>
2025-05-22 14:57:38 -0700

    fixes S3::Util::delete_object

--- lib/s3_util.rb
+++ lib/s3_util.rb
@@ -4,9 +4,15 @@ module S3Util
       key = object_url.match(/(amazonaws\.com|cloudfront\.net)\/(.*)/).try(:[], 2)
       return unless key.present?
 
-      # Do not delete from csp-prod-assets bucket unless in .net production environment
-      if object_url.match(/csp-(dev-assets|staging-assets)/) or ENV['HOST_NAME'] == 'customerstories.net'
-        puts "\n\n\nDeleting object: #{key} from bucket: #{bucket.name}\n\n\n"
+      # Without hardcoding the distribution name, we have no way of knowing if object_url resides in csp-staging-assets or csp-prod-assets.
+      # We want to avoid deleting objects from csp-prod-assets unless we are in the customerstories.net environment,
+      # this because the production db may have been copied to staging or development, including uploaded object links.
+      # The csp-dev-assets bucket is not behind a distribution, so we can identify those objects as development and delete them.
+      # => allow deletion of images residing in csp-dev-assets bucket
+      # => otherwise only allow deletion if in the customerstories.net environment
+      # TODO This means objects updated in the staging environment will not be automatically deleted. Need a cleanup task.
+
+      if object_url.include?('csp-dev-assets') or ENV['HOST_NAME'] == 'customerstories.net'
         res = bucket.delete_objects({
           delete: { 
             objects: [{ key: }] 
