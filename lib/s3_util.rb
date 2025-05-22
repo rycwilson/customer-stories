a03c0e16579a08455f735354052e56268637be95
Ryan Wilson <rycwilson@gmail.com>
2025-05-22 14:31:17 -0700

    adds debug log statement

--- lib/s3_util.rb
+++ lib/s3_util.rb
@@ -3,9 +3,10 @@ module S3Util
     def delete_object(bucket, object_url)
       key = object_url.match(/(amazonaws\.com|cloudfront\.net)\/(.*)/).try(:[], 2)
       return unless key.present?
-      
+
       # Do not delete from csp-prod-assets bucket unless in .net production environment
       if object_url.match(/csp-(dev-assets|staging-assets)/) or ENV['HOST_NAME'] == 'customerstories.net'
+        puts "\n\n\nDeleting object: #{key} from bucket: #{bucket.name}\n\n\n"
         res = bucket.delete_objects({
           delete: { 
             objects: [{ key: }] 
