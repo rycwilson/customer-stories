a30433e36c0f313d52ed73df0bda763a4cf2889d
Ryan Wilson <rycwilson@gmail.com>
2025-06-04 18:34:57 -0700

    modifies companies factory to ensure valid subdomain

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -2,7 +2,13 @@ FactoryBot.define do
   # the class argument can be left out and will be inferred from the factory name
   factory :company, class: 'Company' do
     name { Faker::Company.name }
-    subdomain { Faker::Internet.domain_word }
+    subdomain do
+      loop do
+        candidate = Faker::Internet.domain_word.downcase
+        company = Company.new(subdomain: candidate, name: 'Test', website: 'https://example.com', skip_callbacks: true)
+        break candidate if company.valid?
+      end
+    end
     website { "https://example.com?q=#{Faker::Internet.slug}" }    # needs to be real site, but also unique
     skip_callbacks { true }
 
