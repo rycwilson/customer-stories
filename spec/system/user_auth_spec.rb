ff97fccec2cc9a1f3974cd8999b73346e6c2de3e
Ryan Wilson <rycwilson@gmail.com>
2025-06-05 08:58:41 -0700

    adds example to user auth system spec

--- spec/system/user_auth_spec.rb
+++ spec/system/user_auth_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-def sign_in_as user, password = 'password'
+PASSWORD = SecureRandom.alphanumeric(8)
+
+def sign_in_as user, password = PASSWORD
   fill_in 'Email', with: user.email
   fill_in 'Password', with: password
   click_button 'Sign in'
@@ -8,11 +10,10 @@ end
 
 RSpec.describe 'User authentication', type: :system do
   let(:company) { create(:company) }
-  let(:other_company) { create(:company) }
-
+  
   # The let block will be evaluated in the context of the example,
   # e.g. if `company` is already memoized in the example, it will use that instance here.
-  let(:user) { create(:user, password: 'password', company: company) }
+  let(:user) { create(:user, password: PASSWORD, company:) }
 
   it 'allows the user to sign in without a subdomain' do
     visit new_csp_user_session_path
@@ -27,6 +28,7 @@ RSpec.describe 'User authentication', type: :system do
   end
 
   it "does not allow the user to sign in to another company's subdomain" do
+    other_company = create(:company)
     visit new_csp_user_session_url(subdomain: other_company.subdomain)
     other_company_host = URI.parse(current_url).host
     sign_in_as user
@@ -37,7 +39,7 @@ RSpec.describe 'User authentication', type: :system do
     visit new_csp_user_session_path
     sign_in_as user
     find('.user-profile.dropdown').click
-    click_link 'Sign out'
+    within('.user-profile.dropdown > .dropdown-menu') { click_button 'Sign out' }
     expect(page).to have_current_path(root_url(subdomain: user.company.subdomain), url: true)
   end
 end
