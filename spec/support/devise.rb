5af365af521b4eaf74f54f0cd28851e81c641ec9
Ryan Wilson <rycwilson@gmail.com>
2025-06-07 11:53:45 -0700

    adds devise helpers to system tests

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -4,6 +4,7 @@ require_relative 'controller_macros.rb'
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::IntegrationHelpers, type: :system
   # config.extend ControllerMacros, :type => :request
   # config.include Devise::Test::ControllerHelpers, type: :controller
   # config.include Devise::Test::IntegrationHelpers, type: :view
