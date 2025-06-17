356f099af2ec7355f245fa20febba75533aead3d
Ryan Wilson <rycwilson@gmail.com>
2025-06-17 14:15:48 -0700

    removes unused from sprockets manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,24 +1,7 @@
 //= link_tree ../images
 
+//= link_tree ../builds
+
 //= link mvpready_core.js
 //= link mvpready_helpers.js
 //= link mvpready_landing.js
-//= link_tree ../builds
-
-// SASS compilation now happens in the build step, so we don't need to link stylesheets here.
-/* link_directory ../stylesheets .css */
-
-/*
-This is the same thing that is specified in assets.rb
-Note that link_directory does not distinguish between css and scss files
-link custom/centerforcustomerengagement/stories.css
-link custom/compas/stories.css
-link custom/pixlee/app.css
-link custom/pixlee/stories.css
-link custom/pixlee/plugins.css
-link custom/trunity/stories.css
-link custom/trunity/plugins.css
-link custom/varmour/app.css
-link custom/varmour/stories.css
-link custom/varmour/plugins.css
-*/
