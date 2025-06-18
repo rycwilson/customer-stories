55d34c7e220118212ebfadeed38d419b5187547f
Ryan Wilson <rycwilson@gmail.com>
2025-06-18 09:56:25 -0700

    removes assets from sprockets precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # ref: https://stackoverflow.com/questions/10905905/using-fonts-with-rails-asset-pipeline
@@ -14,17 +12,4 @@ Rails.application.config.assets.precompile << ['*.svg', '*.eot', '*.woff', '*.wo
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(
-  mvpready-landing.css
-  google/ads_preview.js
-  google/ads_preview.css
-  jquery-textfill/source/jquery.textfill.js
-  plugins/cs_overlays.js
-)
-
-# custom stylesheets
-Dir.glob("#{Rails.root.join('app', 'assets', 'stylesheets', 'custom')}/**/*.css") do |file_path|
-  company_subdomain = file_path.match(/custom\/(?<subd>[a-z0-9-]+)\//)['subd']
-  Rails.application.config.assets.precompile << "custom/#{company_subdomain}/#{File.basename(file_path)}"
-end
+Rails.application.config.assets.precompile += %w plugins/cs_overlays.js
