9222aa96ac17a9fe277d2776aad41759ee3afffe
Ryan Wilson <rycwilson@gmail.com>
2025-06-18 09:43:36 -0700

    modifies build scripts and Procfile.dev

--- sass-build.sh
+++ sass-build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e  # Exit on error
 
 FLAGS="
   --load-path=node_modules
@@ -13,13 +14,34 @@ FLAGS="
 "
 # --no-source-map
 
-if [ "$1" = "--custom" ]; then
-  shift
+# Remove --custom from arguments, rebuild $@
+CUSTOM_FLAG=0
+ARGS=
+for arg in "$@"; do
+  if [ "$arg" = "--custom" ]; then
+    CUSTOM_FLAG=1
+  else
+    ARGS="$ARGS \"$(printf '%s' "$arg" | sed 's/"/\\"/g')\""
+  fi
+done
+
+# Reset positional parameters to ARGS (without --custom)
+# shellcheck disable=SC2086
+eval set -- $ARGS
+
+if [ "$CUSTOM_FLAG" -eq 1 ]; then
+  PAIRS=
   for f in app/assets/stylesheets/custom/*/*main*.scss; do
+    [ -f "$f" ] || continue
     subd=$(basename "$(dirname "$f")")
+    outdir="app/assets/builds/custom/$subd"
     outname=$(basename "${f%.scss}" | sed 's/_main//')
-    sass $FLAGS "$f" "app/assets/builds/custom/$subd/${outname}.css" "$@"
+    mkdir -p "$outdir"
+    PAIRS="$PAIRS \"$f\":\"$outdir/${outname}.css\""
   done
+  # shellcheck disable=SC2086
+  eval sass $FLAGS $PAIRS "$@"
 else
+  # shellcheck disable=SC2086
   sass $FLAGS "$@"
 fi
