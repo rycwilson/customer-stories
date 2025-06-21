fa813f6e0b458f52a8e2ef3c5736536c3bd7c323
Ryan Wilson <rycwilson@gmail.com>
2025-06-20 19:16:17 -0700

    removes obsoleted

--- config/initializers/active_record_belongs_to_required_by_default.rb
+++
@@ -1,3 +0,0 @@
-# Not sure why this was originally added, but it needs to be false (set in new_framework_defaults.rb),
-# since, e.g., a User model can exist without a company_id
-# Rails.application.config.active_record.belongs_to_required_by_default = true
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,12 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# ref: https://stackoverflow.com/questions/10905905/using-fonts-with-rails-asset-pipeline
-# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)\z/
-# Rails.application.config.assets.precompile << ['*.svg', '*.eot', '*.woff', '*.woff2', '*.ttf', '*.otf']
-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( 
-#   plugins/cs_overlays.js
-# )
+# Rails.application.config.assets.precompile += %w()
