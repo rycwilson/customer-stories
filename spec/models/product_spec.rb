62504dd70bfbea8d363c79423c87d695b5ff5e40
Ryan Wilson <rycwilson@gmail.com>
2025-06-22 13:17:56 -0700

    adds to product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   subject(:product) { build(:product) }
+  let(:existing_company) { create(:company) }
 
   describe 'factory' do
     it { is_expected.to be_valid }
@@ -15,14 +16,20 @@ RSpec.describe Product, type: :model do
     it 'requires a name' do
       product.name = nil
       expect(product).to be_invalid
-      expect(product.errors[:name]).to include('is required')
+      expect(product.errors[:name]).to include(I18n.t('activerecord.errors.messages.blank'))
     end
 
     it 'requires a unique name within the scope of a company' do
-      existing_category = create(:product, name: 'Unique Product', company: product.company)
-      product.name = existing_category.name
+      existing_product = create(:product, name: 'Unique Product', company: product.company)
+      product.name = existing_product.name
       expect(product).to be_invalid
-      expect(product.errors[:name]).to include('has already been taken')
+      expect(product.errors[:name]).to include(I18n.t('errors.messages.taken'))
+    end
+
+    it 'allows duplicated names outside the scope of a company' do
+      existing_product = create(:product, name: 'Duplicated Product', company: existing_company)
+      product.name = existing_product.name
+      expect(product).to be_valid
     end
   end
 end
