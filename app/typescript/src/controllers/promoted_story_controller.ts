eac75390481e527257dd2b84b736dd195ca75b33
Ryan Wilson <rycwilson@gmail.com>
2025-06-24 09:07:34 -0700

    modifies rubocop settings

--- app/typescript/src/controllers/promoted_story_controller.ts
+++ app/typescript/src/controllers/promoted_story_controller.ts
@@ -27,17 +27,18 @@ export default class PromotedStoryController extends DatatableRowController<Prom
   }
 
   async updateStatus({ detail: { state: shouldEnable } }: { detail: { state: boolean } }) {
+    const newStatus = shouldEnable ? 'ENABLED' : 'PAUSED';
     this.$statusSwitch.bootstrapSwitch('disabled', true);
     this.statusLabelTarget.textContent = '\u00A0';
     const request = new FetchRequest('patch', this.path, {
-      body: { adwords_ad: { status: shouldEnable ? 'ENABLED' : 'PAUSED' } },
+      body: { adwords_ad: { status: newStatus } },
       responseKind: 'turbo-stream'
     });
     const response = await request.perform();
     this.$statusSwitch.bootstrapSwitch('disabled', false);
     if (response.ok) {
-      this.statusLabelTarget.textContent = shouldEnable ? 'ENABLED' : 'PAUSED';
-      this.updateRow({ status: shouldEnable ? 'ENABLED' : 'PAUSED' });
+      this.statusLabelTarget.textContent = newStatus;
+      this.updateRow({ status: newStatus });
     } else {
       this.$statusSwitch.bootstrapSwitch('state', !shouldEnable, true);
       this.statusLabelTarget.textContent = 'ERROR';
