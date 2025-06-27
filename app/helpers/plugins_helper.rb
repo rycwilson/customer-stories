dd737d7036f36e9c7bc5370fb458085062f4a2a3
Ryan Wilson <rycwilson@gmail.com>
2025-06-26 18:23:20 -0700

    moves grouped_options_for_select arg into helper

--- app/helpers/plugins_helper.rb
+++ app/helpers/plugins_helper.rb
@@ -1,12 +1,14 @@
-module PluginsHelper
+# frozen_string_literal: true
 
+module PluginsHelper
   def demo_class_name(company, plugin)
-    "#{company.subdomain} #{plugin[:type].sub('_', '-')} #{plugin[:carousel] ? 'bg-' + plugin[:carousel][:background] : ''}"
+    plugin_type = plugin[:type].sub('_', '-')
+    "#{company.subdomain} #{plugin_type} #{plugin[:carousel] ? "bg-#{plugin[:carousel][:background]}" : ''}"
   end
 
   # method provides for auto-populating settings for legacy plugins that won't
   # necessarily have the expected data attributes
-  def tabbed_carousel_style (company, tab_color, text_color, border_only=false)
+  def tabbed_carousel_style(company, tab_color, text_color, border_only = false)
     if border_only
       "border-top-color: #{tab_color}"
     else
@@ -14,4 +16,11 @@ module PluginsHelper
     end
   end
 
+  def featured_stories_grouped_by_customer(company)
+    company.customers.filter_map do |customer|
+      if customer.stories.featured.present?
+        [customer.name, customer.stories.featured.map { |story| [story.title, story.id] }]
+      end
+    end.to_h
+  end
 end
