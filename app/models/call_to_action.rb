491c78bb10bbf7a30786912ec3e6c13017715d77
Ryan Wilson <rycwilson@gmail.com>
2025-06-28 09:42:36 -0700

    adds cta validations

--- app/models/call_to_action.rb
+++ app/models/call_to_action.rb
@@ -6,10 +6,23 @@ class CallToAction < ApplicationRecord
   has_many :stories, through: :successes
   accepts_nested_attributes_for(
     :company,
-    reject_if: ->(attrs) { !attrs.keys.all? { |attr| attr.match(/(\Aid\z)|primary_cta/) } }
+    reject_if: ->(company_attrs) { !color_attributes_only?(company_attrs) },
+    allow_destroy: false
   )
+
+  validates :display_text, :company, presence: true
+  validates :link_url, presence: true, if: -> { type == 'CtaLink' }
+
   scope :primary, -> { where(primary: true) }
-  scope :sidebar, -> { where(primary: false) }
+  scope :sidebar, -> { where(primary: false) } # where.not(id: primary)
   scope :links, -> { where(type: 'CtaLink') }
   scope :forms, -> { where(type: 'CtaForm') }
+
+  # Primary CTA colors are defined on the company.
+  # Any update should include only the company id and primary_cta_* attributes.
+  def self.color_attributes_only?(attrs)
+    attrs.keys.all? { |attr| attr =~ /(\Aid\z)|primary_cta/ }
+  end
+
+  private_class_method :color_attributes_only?
 end
