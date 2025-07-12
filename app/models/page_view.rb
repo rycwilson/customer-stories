f0bc3cbdef004e3d62a1698a5f0be7a1fc833dec
Ryan Wilson <rycwilson@gmail.com>
2025-07-11 19:46:53 -0700

    refactors scopes

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -3,18 +3,4 @@
 class PageView < VisitorAction
   scope :story, -> { joins(:success) } # excludes index page views
   scope :since, ->(date) { joins(:visitor_session).where(visitor_sessions: { timestamp: date... }) }
-
-  # NOTE: Any field that is selected and not aggregated must be included in the GROUP BY clause.
-  # Use a left_outer_joins to account for some page views being tied to stories#index
-  scope :visitors_by_story, lambda {
-    left_outer_joins(success: %i[customer story])
-      .joins(visitor_session: :visitor)
-      .group('customers.name, stories.title')
-      .select(Arel.sql([
-        'customers.name AS customer',
-        'stories.title',
-        'COUNT(DISTINCT visitors.id) AS visitors'
-      ].join(', ')))
-      .order('visitors DESC')
-  }
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -13,4 +13,44 @@ class Visitor < ApplicationRecord
 
   scope :to_company, ->(company_id) { joins(:page_views).where(visitor_actions: { company_id: }).distinct }
   scope :top_to_company, ->(company_id, top) { to_company(company_id).order(visitor_sessions_count: :desc).limit(top) }
+
+  scope :to_company_by_date, lambda { |company_id, **options|
+    story = options[:story]
+    start_date = options[:start_date] || 30.days.ago
+    end_date = options[:end_date] || Date.today
+    days_between = (end_date.to_date - start_date.to_date).to_i
+    group_by, group_range =
+      case days_between
+      when 0...21
+        ['day', start_date.beginning_of_day..end_date.end_of_day]
+      when 21...120
+        ['week', start_date.beginning_of_week.beginning_of_day...end_date.end_of_week.end_of_day]
+      else
+        ['month', start_date.beginning_of_month.beginning_of_day...end_date.end_of_month.end_of_day]
+      end
+    date_trunc = "DATE_TRUNC('#{group_by}', visitor_sessions.timestamp)"
+    where_conditions = {
+      visitor_sessions: { timestamp: group_range },
+      visitor_actions: { company_id: }
+    }
+    where_conditions[:stories] = { id: story.id } if story.present?
+    select("#{date_trunc} AS date, COUNT(DISTINCT visitors.id) AS visitors")
+      .joins(visitor_sessions: { visitor_actions: { success: :story } })
+      .where(where_conditions)
+      .group(date_trunc)
+      .order('date ASC')
+  }
+
+  scope :to_company_by_story, lambda { |company_id|
+    joins(visitor_sessions: { visitor_actions: { success: %i[customer story] } })
+      .where(visitor_actions: { company_id: })
+      .group('customers.name, stories.title, visitor_actions.company_id')
+      .select([
+        'customers.name AS customer',
+        'stories.title',
+        'visitor_actions.company_id',
+        'COUNT(DISTINCT visitors.id) AS visitors'
+      ].join(', '))
+      .order('visitors DESC')
+  }
 end
