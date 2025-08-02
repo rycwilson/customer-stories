063e279127d502f26125ed97b8d8fba6c847e182
Ryan Wilson <rycwilson@gmail.com>
2025-08-02 12:00:19 -0700

    adds tooltips to dashboard nav items

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -40,4 +40,17 @@ module CompaniesHelper
     end
   end
 
+  def nav_link_tooltip_options(title)
+    {
+      title:,
+      placement: 'right',
+      delay: { show: 700, hide: 0 },
+      template: <<~HTML.squish
+        <div class="tooltip" role="tooltip">
+          <!-- <div class="tooltip-arrow"></div> -->
+          <div class="tooltip-inner"></div>
+        </div>
+      HTML
+    }
+  end
 end
--- app/typescript/src/controllers/tooltip_controller.ts
+++ app/typescript/src/controllers/tooltip_controller.ts
@@ -2,8 +2,8 @@ import { Controller } from '@hotwired/stimulus';
 
 interface TooltipOptions {
   title: string,
+  container: string,
   placement?: string,
-  container?: string,
 }
 
 const baseOptions: TooltipOptions = { title: 'I am a tooltip', container: 'body' };
@@ -13,17 +13,53 @@ export default class TooltipController extends Controller {
     options: { type: Object, default: {} }
   }
   declare optionsValue: TooltipOptions;
+  
+  declare navItemObserver: MutationObserver | undefined;
+
+  get tooltipElement() {
+    return $(this.element).data('bs.tooltip').$tip;
+  }
 
   connect() {
     $(this.element).tooltip({ ...baseOptions, ...this.optionsValue });
+
+    // Don't show tooltip on active tab
+    if (this.element.role === 'tab') {
+      const navItem = this.element.parentElement;
+      if (navItem instanceof HTMLLIElement) {
+        this.toggleNavLink(navItem);
+        this.navItemObserver = new MutationObserver(() => this.toggleNavLink(navItem));
+        this.navItemObserver.observe(navItem, { attributes: true, attributeFilter: ['class'] });
+      }
+    }
   }
 
   disconnect() {
     $(this.element).tooltip('destroy');
+    this.navItemObserver?.disconnect();
   }
 
-  optionsValueChanged(newOptions: TooltipOptions) {
+  optionsValueChanged(options: TooltipOptions) {
     $(this.element).tooltip('destroy');
-    $(this.element).tooltip({ ...baseOptions, ...newOptions });
+    $(this.element).tooltip({ ...baseOptions, ...options });
+  }
+
+  toggleNavLink(navItem: HTMLLIElement) {
+    // This approach results in erratic hover and tooltip behavior in the first nav item
+    // if (navItem.classList.contains('active')) {
+    //   $(this.element).tooltip('hide');
+    //   $(this.element).tooltip('disable')
+    // } else {
+    //   $(this.element).tooltip('enable');
+    // }
+
+    // Instead:
+    $(this.element).tooltip('destroy');
+    if (navItem.classList.contains('active')) return;
+    $(this.element).tooltip({ 
+      ...baseOptions, 
+      ...this.optionsValue,
+      container: this.element.closest('.sidebar--sans-text')
+    });
   }
 }
