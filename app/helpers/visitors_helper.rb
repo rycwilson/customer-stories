1bacb8b1c4b6b86e81ce664e52de87b3a6b0acad
Ryan Wilson <rycwilson@gmail.com>
2025-10-15 09:35:18 -0700

    adds visitors filters helper method

--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module VisitorsHelper
   def visitors_display_options_escaped_html(company, curator_id = nil)
     html = render(
@@ -8,5 +10,11 @@ module VisitorsHelper
       }
     )
     escape_once(html)
+  end
+
+  def visitors_filters(curator_id)
+    {
+      'curator-id': curator_id
+    }
   end
 end
