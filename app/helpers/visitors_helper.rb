b1b6d7a646a8df8f0a5406e9fd6e379f49d1e663
Ryan Wilson <rycwilson@gmail.com>
2025-10-16 11:35:40 -0700

    refactors

--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -2,13 +2,7 @@
 
 module VisitorsHelper
   def visitors_display_options_escaped_html(company, curator_id = nil)
-    html = render(
-      'visitors/display_options',
-      {
-        company:,
-        curator_id:,
-      }
-    )
+    html = render('visitors/display_options', { company:, curator_id: })
     escape_once(html)
   end
 
