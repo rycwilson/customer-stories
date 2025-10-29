f2d15b5260e2b66cc65784933c7a246bc261bfe1
Ryan Wilson <rycwilson@gmail.com>
2025-10-28 18:13:50 -0700

    removes unused helper method

--- app/helpers/visitors_helper.rb
+++ app/helpers/visitors_helper.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
 module VisitorsHelper
-  def visitors_display_options_escaped_html(company, curator_id = nil)
-    html = render('visitors/display_options', { company:, curator_id: })
+  def visitors_display_options_html(company, filters)
+    html = render('visitors/display_options', company:, filters:)
     escape_once(html)
-  end
-
-  def visitors_filters(curator_id)
-    {
-      'curator': curator_id
-    }
   end
 end
