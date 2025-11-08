49cb52c8597515ae4eb9102b521186daea072ac3
Ryan Wilson <rycwilson@gmail.com>
2025-11-08 12:18:50 -0800

    refactors

--- app/typescript/src/controllers/resource_controller.ts
+++ app/typescript/src/controllers/resource_controller.ts
@@ -50,21 +50,22 @@ export default class ResourceController extends Controller<HTMLElement> {
   }
 
   initValueChanged(shouldInit: boolean) {
-    if (shouldInit) {
-      if (this.dataExists) {
+    if (!shouldInit) return;
+
+    if (this.dataExists) {
+      initTable.call(this);
+    } else {
+      this.dispatch('loading');
+      // console.log('getting data:', this.dataPathValue)
+      getJSON(this.dataPathValue).then(data => {
+        console.log('data:', this.resourceName)
+        if (this.resourceName === 'storyContributions') {
+          CSP[this.resourceName][+(this.element.dataset.storyId as string)] = data;
+        } else {
+          CSP[this.resourceName] = data;
+        }
         initTable.call(this);
-      } else {
-        this.dispatch('loading');
-        // console.log('getting data:', this.dataPathValue)
-        getJSON(this.dataPathValue).then(data => {
-          if (this.resourceName === 'storyContributions') {
-            CSP[this.resourceName][+(this.element.dataset.storyId as string)] = data;
-          } else {
-            CSP[this.resourceName] = data;
-          }
-          initTable.call(this);
-        })
-      }
+      })
     }
   }
 
