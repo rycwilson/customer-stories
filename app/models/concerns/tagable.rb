6db958729eaedc79912fcd3351271ec66f99767e
Ryan Wilson <rycwilson@gmail.com>
2026-05-21 12:18:59 -0700

    adds Tagable concern

---
+++ app/models/concerns/tagable.rb
@@ -0,0 +1,23 @@
+module Tagable 
+  extend ActiveSupport::Concern
+
+  included do
+    include FriendlyId
+
+    belongs_to :company
+    has_and_belongs_to_many :successes
+    has_and_belongs_to_many :contributor_questions, dependent: :destroy
+    has_many :stories, through: :successes
+    has_many :customers, through: :successes
+
+    validates :name, presence: true, uniqueness: { scope: :company }
+
+    scope :featured, -> { joins(:stories).merge(Story.featured.reorder(nil)).distinct }
+
+    friendly_id :name, use: %i[slugged scoped], scope: :company_id
+  end
+
+  def should_generate_new_friendly_id?
+    new_record? || name_changed? || slug.blank?
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,5 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  include FriendlyId
-
-  belongs_to :company
-  has_and_belongs_to_many :successes
-  has_and_belongs_to_many :contributor_questions, dependent: :destroy
-  has_many :stories, through: :successes
-  has_many :customers, through: :successes
-
-  validates :name, presence: true, uniqueness: { scope: :company }
-
-  scope :featured, -> { joins(:stories).merge(Story.featured.reorder(nil)).distinct }
-
-  friendly_id :name, use: %i[slugged scoped], scope: :company_id
-
-  def should_generate_new_friendly_id?
-    new_record? || name_changed? || slug.blank?
-  end
+  include Tagable
 end
