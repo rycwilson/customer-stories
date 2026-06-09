43ffad488727d4a9d2105f8da499e4782a588e28
Ryan Wilson <rycwilson@gmail.com>
2026-06-09 16:59:56 -0700

    adds simplecov init

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,12 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+# 
+
+require 'simplecov'
+SimpleCov.start
+
+# Previous content of test helper now starts here
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
@@ -44,9 +50,10 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
-=begin
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
+=begin  
+  
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
