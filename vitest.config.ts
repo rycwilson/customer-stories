6914bd5b65d5bd3d4bd9918f91a48a1850508cd8
Ryan Wilson <rycwilson@gmail.com>
2026-06-10 14:38:02 -0700

    upgrades vitest and adds coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     setupFiles: ['app/typescript/test/setup.ts'],
     environment: 'jsdom',
     // reporters: ['default'],
+    coverage: {
+      provider: 'v8' // or 'istanbul'
+    },
 
     // When type-checking is run explicitly via `vitest typecheck` use the path to config 
     // defined here. Editor type-checking will find tsconfig.test.json via tsserver traversal to 
