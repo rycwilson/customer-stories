1d3b97b2d64b1d3b2a50d72386df5ce212638107
Ryan Wilson <rycwilson@gmail.com>
2026-08-27 10:23:34 -0700

    removes unused

--- app/typescript/src/controllers/contributor_prompts_controller.ts
+++ app/typescript/src/controllers/contributor_prompts_controller.ts
@@ -1,4 +1,3 @@
-import type { TurboSubmitStartEvent, TurboSubmitEndEvent, FormSubmission } from '@hotwired/turbo';
 import FormController from './form_controller';
 
 export default class ContributorPromptsController extends FormController<ContributorPromptsController> {
