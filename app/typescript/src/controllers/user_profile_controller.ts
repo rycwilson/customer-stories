7f846bfa2f20c3320f11c2e6e4edd405e7c229d7
Ryan Wilson <rycwilson@gmail.com>
2026-09-22 14:35:49 -0700

    modifies user profile ui

--- app/typescript/src/controllers/user_profile_controller.ts
+++ app/typescript/src/controllers/user_profile_controller.ts
@@ -1,47 +1,54 @@
 import FormController from './form_controller';
 
 export default class UserProfileController extends FormController<UserProfileController> {
-  static targets = ['userField', 'emailField', 'currentPasswordField', 'passwordField', 'passwordConfirmationField', 'editBtn'];
-  declare readonly userFieldTargets: HTMLInputElement[];
-  declare readonly emailFieldTarget: HTMLInputElement;
-  declare readonly currentPasswordFieldTarget: HTMLInputElement;
-  declare readonly passwordFieldTarget: HTMLInputElement;
-  declare readonly passwordConfirmationFieldTarget: HTMLInputElement;
-  declare readonly editBtnTargets: HTMLElement[];
+  static targets = [
+    'userInput',
+    'emailInput',
+    'currentPasswordInput',
+    'passwordInput',
+    'passwordConfirmationInput',
+    'editButtonWrapper'
+  ];
+  declare readonly userInputTargets: HTMLInputElement[];
+  declare readonly emailInputTarget: HTMLInputElement;
+  declare readonly currentPasswordInputTarget: HTMLInputElement;
+  declare readonly passwordInputTarget: HTMLInputElement;
+  declare readonly passwordConfirmationInputTarget: HTMLInputElement;
+  declare readonly editButtonWrapperTargets: HTMLElement[];
 
   get hasInvalidNewPassword() {
-    return !this.passwordFieldTarget.checkValidity();
+    return !this.passwordInputTarget.checkValidity();
   }
 
   get passwordsDoNotMatch() {
-    return this.passwordFieldTarget.value !== this.passwordConfirmationFieldTarget.value;
+    return this.passwordInputTarget.value !== this.passwordConfirmationInputTarget.value;
   }
   
-  resetUserFields() {
-    this.userFieldTargets.forEach(input => {
+  resetUserInputs() {
+    this.userInputTargets.forEach(input => {
       input.value = <string>input.dataset.initialValue;
       input.disabled = true;
     });
   }
   
   enableProtectedField(input: HTMLInputElement) {
-    this.resetUserFields();
-    this.editBtnTargets.forEach(inputGroupBtn => inputGroupBtn.remove());
-    this.currentPasswordFieldTarget.value = '';
-    this.currentPasswordFieldTarget.disabled = false;
+    this.resetUserInputs();
+    this.editButtonWrapperTargets.forEach(div => div.remove());
+    this.currentPasswordInputTarget.value = '';
+    this.currentPasswordInputTarget.disabled = false;
     input.disabled = false;
   }
 
-  changeEmail(e: Event) {
-    this.enableProtectedField(this.emailFieldTarget);
+  changeEmail(_e: CustomEvent) {
+    this.enableProtectedField(this.emailInputTarget);
   }
 
-  changePassword(e: Event) {
-    this.enableProtectedField(this.currentPasswordFieldTarget);
-    [this.passwordFieldTarget, this.passwordConfirmationFieldTarget].forEach(input => input.disabled = false);
+  changePassword(_e: CustomEvent) {
+    this.enableProtectedField(this.currentPasswordInputTarget);
+    [this.passwordInputTarget, this.passwordConfirmationInputTarget].forEach(input => input.disabled = false);
     this.element.classList.add('has-new-password');
     this.submitBtnTarget.value = 'Change password';
-    setTimeout(() => this.currentPasswordFieldTarget.focus()); // focus after the DOM update
+    setTimeout(() => this.currentPasswordInputTarget.focus()); // focus after the DOM update
   }
 
   validate(e: SubmitEvent) {
@@ -49,19 +56,19 @@ export default class UserProfileController extends FormController<UserProfileCon
 
     // We want to validate the password confirmation separately so as to give it a single help message (.help-block element),
     // i.e. presence and format errors can be flagged on the password input alone, and not repeated for the confirmation input
-    if (this.passwordFieldTarget.disabled === false) {
-      const formGroup = this.passwordConfirmationFieldTarget.closest('.form-group')!;
+    if (this.passwordInputTarget.disabled === false) {
+      const formGroup = this.passwordConfirmationInputTarget.closest('.form-group')!;
       if (isValid) {
         if (this.passwordsDoNotMatch) {
           e.preventDefault();
           e.stopPropagation();
           isValid = false;
           formGroup.classList.add('has-error');
-          this.passwordConfirmationFieldTarget.focus();
+          this.passwordConfirmationInputTarget.focus();
         } 
       } else if (this.hasInvalidNewPassword) {
         formGroup.classList.remove('has-error');
-        this.passwordConfirmationFieldTarget.value = '';
+        this.passwordConfirmationInputTarget.value = '';
       }
     }
     return isValid;
