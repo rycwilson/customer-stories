746388af63b089cf1e0afe508fd6618d691dc86e
Ryan Wilson <rycwilson@gmail.com>
2026-09-24 08:26:48 -0700

    adds a comment

--- lib/company_subdomain.rb
+++ lib/company_subdomain.rb
@@ -2,6 +2,9 @@
 
 class CompanySubdomain
   def self.matches?(request)
+    # TODO: Save { subdomain => company_id } to Redis
+    # memory_store = {}
+    # company_exists = memory_store[request.subdomain] || Company.exists?(subdomain: request.subdomain)
     request.subdomain.present? and Company.exists?(subdomain: request.subdomain)
   end
 end
